8243d6e96ca497fc7ec0456814b8f4e98c8e1ff6
crategus <crategus@users.sourceforge.net>
2012-02-01 00:14:58 +0100

    Adding more Lisp support for the GTKUIManager

--- gtk/gtk.ui-manager.lisp
+++ gtk/gtk.ui-manager.lisp
@@ -457,6 +457,168 @@
 
 (in-package :gtk)
 
+(defstruct ui-d
+  class
+  props
+  children
+  expansion
+  var
+  initform
+  initializer)
+
+(defstruct ui-prop
+  name
+  value)
+
+(defstruct ui-child
+  v
+  props)
+
+(defun parse-ui-props (list)
+  ;; list is ({:prop value}* rest)
+  (iter (for x first list then (cddr x))
+        (while (keywordp (first x)))
+        (for (name value) = x)
+        (collect (make-ui-prop :name name :value value) into props)
+        (finally (return (values props x)))))
+
+(defun parse-ui-children (list)
+  ;; list is (child*)
+  ;; child is {ui {:prop value}*}
+  (iter (while list)
+        (for child = (if (eq :expr (first (first list)))
+                         (make-ui-d :var (second (first list)))
+                         (parse-ui-description (first list))))
+        (for (values props rest) = (parse-ui-props (rest list)))
+        (setf list rest)
+        (collect (make-ui-child :v child :props props))))
+
+(defun parse-ui-description (description)
+  ;; description is (class {:prop value}* child*)
+  ;; child is {ui {:prop value}*}
+  (let ((class (first description)))
+    (multiple-value-bind (props rest) (parse-ui-props (rest description))
+      (let ((children (parse-ui-children rest)))
+        (make-ui-d :class class :props props :children children)))))
+
+(defun get-ui-d-var (d)
+  (let ((prop (find :var (ui-d-props d) :key #'ui-prop-name)))
+    (if prop
+        (ui-prop-value prop)
+        (gensym (format nil "~A-" (symbol-name (ui-d-class d)))))))
+
+(defun get-ui-d-initform (d)
+  `(make-instance ',(ui-d-class d)
+                  ,@(iter (for prop in (ui-d-props d))
+                          (unless (eq (ui-prop-name prop) :var)
+                            (appending (list (ui-prop-name prop)
+                                             (ui-prop-value prop)))))))
+
+(defgeneric pack-child (container child &key))
+
+(defmethod pack-child ((w gtk-container) child &key)
+  (gtk-container-add w child))
+
+(defmethod pack-child ((b gtk-box) child &key (expand t)
+                                              (fill t)
+                                              (padding 0) pack-type position)
+  (gtk-box-pack-start b child :expand expand :fill fill :padding padding)
+  (when pack-type
+    (setf (gtk-box-child-pack-type b child) pack-type))
+  (when position
+    (setf (gtk-box-child-position b child) position)))
+
+(defmethod pack-child ((p gtk-paned) child &key (resize 'default) (shrink t))
+  (if (null (gtk-paned-child-1 p))
+      (gtk-paned-pack-1 p child
+		    :resize (if (eq resize 'default) nil resize)
+		    :shrink shrink)
+      (gtk-paned-pack-2 p child
+		    :resize (if (eq resize 'default) t resize)
+		    :shrink shrink)))
+
+(defmethod pack-child ((table gtk-table) child &key
+		       left right top bottom
+                       (x-options '(:expand :fill))
+                       (y-options '(:expand :fill))
+                       (x-padding 0) (y-padding 0))
+  (unless left
+    (error "left is a mandatory child property for table packing"))
+  (unless right
+    (error "right is a mandatory child property for table packing"))
+  (unless top
+    (error "top is a mandatory child property for table packing"))
+  (unless bottom
+    (error "bottom is a mandatory child property for table packing"))
+  (gtk-table-attach table child
+                    left
+                    right
+                    top
+                    bottom
+                    :x-options x-options
+                    :y-options y-options
+                    :x-padding x-padding
+                    :y-padding y-padding))
+
+(defmethod pack-child ((w gtk-tree-view) child &key)
+  (gtk-tree-view-append-column w child))
+
+(defmethod pack-child ((w gtk-tree-view-column) child
+                       &key (expand t) attributes)
+  (gtk-tree-view-column-pack-start w child :expand expand)
+  (iter (for a on attributes by #'cddr)
+        (gtk-tree-view-column-add-attribute w
+                                            child
+					    (first a)
+					    (second a))))
+
+(defmethod pack-child ((b gtk-toolbar) child &key (expand 'default)
+                                              (homogeneous 'default))
+  (gtk-toolbar-insert b child -1)
+  (unless (eq expand 'default)
+    (container-call-set-property b child "expand" expand +g-type-boolean+))
+  (unless (eq homogeneous 'default)
+    (container-call-set-property b
+                                 child
+                                 "homogeneous" homogeneous
+                                 +g-type-boolean+)))
+
+(defun set-ui-expansion-1 (d)
+  (when (ui-d-class d)
+    ;; only direct-vars do not have class
+    (setf (ui-d-var d) (get-ui-d-var d)
+          (ui-d-initform d) (get-ui-d-initform d))))
+
+(defun set-ui-expansion (description)
+  (iter (for child in (ui-d-children description))
+        (set-ui-expansion (ui-child-v child)))
+  (set-ui-expansion-1 description))
+
+(defun flattened-ui-descriptions (d)
+  (cons d
+        (iter (for child in (ui-d-children d))
+              (when (ui-d-class (ui-child-v child))
+                (appending (flattened-ui-descriptions (ui-child-v child)))))))
+
+(defmacro let-ui (ui-description &body body)
+  (let* ((description (parse-ui-description ui-description))
+         (items (flattened-ui-descriptions description)))
+    (set-ui-expansion description)
+    `(let (,@(iter (for item in items)
+                   (collect (list (ui-d-var item)
+                                  (ui-d-initform item)))))
+       ,@(iter (for item in items)
+	       (appending (iter (for child in (ui-d-children item))
+				(for child-var = (ui-d-var (ui-child-v child)))
+				(let ((props
+				       (iter (for p in (ui-child-props child))
+				             (appending (list (ui-prop-name p)
+				                              (ui-prop-value p))))))
+				  (collect (list* 'pack-child
+				                  (ui-d-var item)
+				                  child-var props))))))
+       ,@body)))
+
 ;;; ----------------------------------------------------------------------------
 ;;; struct GtkUIManager
 ;;; 
