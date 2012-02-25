8d12ef0988496f9b01e3004e43dab0da4ff97b9e
crategus <crategus@users.sourceforge.net>
2012-02-25 18:20:03 +0100

    Moving definitions to other files

--- gtk/gtk.generated-classes.lisp
+++ gtk/gtk.generated-classes.lisp
@@ -43,14 +43,6 @@
   (:vertical 2)
   (:both 3))
 
-(define-g-enum "GtkPackDirection"
-    pack-direction
-    (:export t :type-initializer "gtk_pack_direction_get_type")
-  (:ltr 0)
-  (:rtl 1)
-  (:ttb 2)
-  (:btt 3))
-
 (define-g-enum "GtkSensitivityType"
     sensitivity-type
     (:export t :type-initializer "gtk_sensitivity_type_get_type")
@@ -135,12 +127,6 @@
   (:as-child 1)
   (:after 2))
 
-(define-g-enum "GtkIconThemeError"
-    icon-theme-error
-    (:export t :type-initializer "gtk_icon_theme_error_get_type")
-  (:not-found 0)
-  (:failed 1))
-
 (define-g-enum "GtkMatchType"
     match-type
     (:export t :type-initializer "gtk_match_type_get_type")
@@ -156,50 +142,6 @@
     (:export t :type-initializer "gtk_preview_type_get_type")
   (:color 0)
   (:grayscale 1))
-
-(define-g-enum "GtkRcTokenType"
-    rc-token-type
-    (:export t :type-initializer "gtk_rc_token_type_get_type")
-  (:invalid 270)
-  (:include 271)
-  (:normal 272)
-  (:active 273)
-  (:prelight 274)
-  (:selected 275)
-  (:insensitive 276)
-  (:fg 277)
-  (:bg 278)
-  (:text 279)
-  (:base 280)
-  (:xthickness 281)
-  (:ythickness 282)
-  (:font 283)
-  (:fontset 284)
-  (:font-name 285)
-  (:bg-pixmap 286)
-  (:pixmap-path 287)
-  (:style 288)
-  (:binding 289)
-  (:bind 290)
-  (:widget 291)
-  (:widget-class 292)
-  (:class 293)
-  (:lowest 294)
-  (:gtk 295)
-  (:application 296)
-  (:theme 297)
-  (:rc 298)
-  (:highest 299)
-  (:engine 300)
-  (:module-path 301)
-  (:im-module-path 302)
-  (:im-module-file 303)
-  (:stock 304)
-  (:ltr 305)
-  (:rtl 306)
-  (:color 307)
-  (:unbind 308)
-  (:last 309))
 
 (define-g-enum "GtkSideType"
     side-type
@@ -278,15 +220,6 @@
   (:printing 1024)
   (:builder 2048))
 
-(define-g-flags "GtkIconLookupFlags"
-    icon-lookup-flags
-    (:export t :type-initializer "gtk_icon_lookup_flags_get_type")
-  (:no-svg 1)
-  (:force-svg 2)
-  (:use-builtin 4)
-  (:generic-fallback 8)
-  (:force-size 16))
-
 (define-g-flags "GtkPrivateFlags"
     private-flags
     (:export t :type-initializer "gtk_private_flags_get_type")
@@ -304,14 +237,6 @@
   (:alloc-needed 4096)
   (:request-needed 8192))
 
-(define-g-flags "GtkRcFlags"
-    rc-flags
-    (:export t :type-initializer "gtk_rc_flags_get_type")
-  (:fg 1)
-  (:bg 2)
-  (:text 4)
-  (:base 8))
-
 (define-g-flags "GtkSignalRunType"
     signal-run-type
     (:export t :type-initializer "gtk_signal_run_type_get_type")
@@ -323,54 +248,6 @@
   (:no-hooks 64))
 
 ;;; ----------------------------------------------------------------------------
-
-(define-g-object-class "GtkFontButton" gtk-font-button
-  (:superclass gtk-button
-    :export t
-    :interfaces ("AtkImplementorIface" "GtkActivatable" "GtkBuildable")
-    :type-initializer "gtk_font_button_get_type")
-  ((font-name
-    gtk-font-button-font-name
-    "font-name" "gchararray" t t)
-   (show-size
-    gtk-font-button-show-size
-    "show-size" "gboolean" t t)
-   (show-style
-    gtk-font-button-show-style
-    "show-style" "gboolean" t t)
-   (title
-    gtk-font-button-title
-    "title" "gchararray" t t)
-   (use-font
-    gtk-font-button-use-font
-    "use-font" "gboolean" t t)
-   (use-size
-    gtk-font-button-use-size
-    "use-size" "gboolean" t t)))
-
-(define-g-object-class "GtkComboBoxEntry" gtk-combo-box-entry
-                       (:superclass gtk-combo-box :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable" "GtkCellEditable"
-                         "GtkCellLayout")
-                        :type-initializer "gtk_combo_box_entry_get_type")
-                       ((text-column combo-box-entry-text-column "text-column"
-                         "gint" t t)))
-
-(define-g-object-class "GtkHandleBox" gtk-handle-box
-                       (:superclass gtk-bin :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_handle_box_get_type")
-                       ((child-detached handle-box-child-detached
-                         "child-detached" "gboolean" t nil)
-                        (handle-position handle-box-handle-position
-                         "handle-position" "GtkPositionType" t t)
-                        (shadow handle-box-shadow "shadow" "GtkShadowType" t t)
-                        (shadow-type handle-box-shadow-type "shadow-type"
-                         "GtkShadowType" t t)
-                        (snap-edge handle-box-snap-edge "snap-edge"
-                         "GtkPositionType" t t)
-                        (snap-edge-set handle-box-snap-edge-set "snap-edge-set"
-                         "gboolean" t t)))
 
 (define-g-object-class "GtkCheckMenuItem" gtk-check-menu-item
                        (:superclass gtk-menu-item :export t :interfaces
@@ -419,121 +296,3 @@
                         :type-initializer "gtk_separator_tool_item_get_type")
                        ((draw separator-tool-item-draw "draw" "gboolean" t t)))
 
-(define-g-object-class "GtkMenuToolButton" gtk-menu-tool-button
-                       (:superclass gtk-tool-button :export t :interfaces
-                        ("AtkImplementorIface" "GtkActivatable" "GtkBuildable")
-                        :type-initializer "gtk_menu_tool_button_get_type")
-                       ((menu menu-tool-button-menu "menu" "GtkMenu" t t)
-                        (:cffi arrow-tooltip-text
-                         menu-tool-button-arrow-tooltip-text :string nil
-                         "gtk_menu_tool_button_set_arrow_tooltip_text")
-                        (:cffi arrow-tooltip-markup
-                         menu-tool-button-arrow-tooltip-markup :string nil
-                         "gtk_menu_tool_button_set_arrow_tooltip_markup")))
-
-(define-g-object-class "GtkColorSelectionDialog" gtk-color-selection-dialog
-                       (:superclass gtk-dialog :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer
-                        "gtk_color_selection_dialog_get_type")
-                       ((cancel-button color-selection-dialog-cancel-button
-                         "cancel-button" "GtkWidget" t nil)
-                        (color-selection color-selection-dialog-color-selection
-                         "color-selection" "GtkWidget" t nil)
-                        (help-button color-selection-dialog-help-button
-                         "help-button" "GtkWidget" t nil)
-                        (ok-button color-selection-dialog-ok-button "ok-button"
-                         "GtkWidget" t nil)))
-
-(define-g-object-class "GtkFontSelectionDialog" gtk-font-selection-dialog
-                       (:superclass gtk-dialog :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_font_selection_dialog_get_type")
-                       ((:cffi font-name font-selection-dialog-font-name
-                         (g-string :free-from-foreign t :free-to-foreign t)
-                         "gtk_font_selection_dialog_get_font_name"
-                         "gtk_font_selection_dialog_set_font_name")
-                        (:cffi preview-text font-selection-dialog-preview-text
-                         :string "gtk_font_selection_dialog_get_preview_text"
-                         "gtk_font_selection_dialog_set_preview_text")
-                        (:cffi apply-button font-selection-dialog-apply-button
-                         g-object "gtk_font_selection_dialog_get_apply_button"
-                         nil)
-                        (:cffi cancel-button
-                         font-selection-dialog-cancel-button g-object
-                         "gtk_font_selection_dialog_get_cancel_button" nil)
-                        (:cffi ok-button font-selection-dialog-ok-button
-                         g-object "gtk_font_selection_dialog_get_ok_button"
-                         nil)))
-
-(define-g-object-class "GtkInputDialog" gtk-input-dialog
-                       (:superclass gtk-dialog :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_input_dialog_get_type")
-                       nil)
-
-(define-g-object-class "GtkPageSetupUnixDialog" gtk-page-setup-unix-dialog
-                       (:superclass gtk-dialog :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer
-                        "gtk_page_setup_unix_dialog_get_type")
-                       ((:cffi page-setup page-setup-unix-dialog-page-setup
-                         (g-object page-setup)
-                         "gtk_page_setup_unix_dialog_get_page_setup"
-                         "gtk_page_setup_unix_dialog_set_page_setup")
-                        (:cffi print-settings
-                         page-setup-unix-dialog-print-settings
-                         (g-object print-settings)
-                         "gtk_page_setup_unix_dialog_get_print_settings"
-                         "gtk_page_setup_unix_dialog_set_print_settings")))
-
-(define-g-object-class "GtkPrintUnixDialog" gtk-print-unix-dialog
-                       (:superclass gtk-dialog :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_print_unix_dialog_get_type")
-                       ((current-page print-unix-dialog-current-page
-                         "current-page" "gint" t t)
-                        (page-setup print-unix-dialog-page-setup "page-setup"
-                         "GtkPageSetup" t t)
-                        (print-settings print-unix-dialog-print-settings
-                         "print-settings" "GtkPrintSettings" t t)
-                        (selected-printer print-unix-dialog-selected-printer
-                         "selected-printer" "GtkPrinter" t nil)))
-
-(define-g-object-class "GtkPlug" gtk-plug
-                       (:superclass gtk-window :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_plug_get_type")
-                       ((embedded plug-embedded "embedded" "gboolean" t nil)
-                        (socket-window plug-socket-window "socket-window"
-                         "GdkWindow" t nil)))
-
-(define-g-object-class "GtkMenuBar" gtk-menu-bar
-                       (:superclass gtk-menu-shell :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_menu_bar_get_type")
-                       ((child-pack-direction menu-bar-child-pack-direction
-                         "child-pack-direction" "GtkPackDirection" t t)
-                        (pack-direction menu-bar-pack-direction
-                         "pack-direction" "GtkPackDirection" t t)))
-
-(define-g-object-class "GtkSocket" gtk-socket
-                       (:superclass gtk-container :export t :interfaces
-                        ("AtkImplementorIface" "GtkBuildable")
-                        :type-initializer "gtk_socket_get_type")
-                       nil)
-
-(define-g-object-class "GtkRcStyle" gtk-rc-style
-                       (:superclass g-object :export t :interfaces nil
-                        :type-initializer "gtk_rc_style_get_type")
-                       nil)
-
-(define-g-object-class "GtkStyle" gtk-style
-                       (:superclass g-object :export t :interfaces nil
-                        :type-initializer "gtk_style_get_type")
-                       nil)
-
-(define-g-object-class "GtkIconTheme" gtk-icon-theme
-                       (:superclass g-object :export t :interfaces nil
-                        :type-initializer "gtk_icon_theme_get_type")
-                       nil)
