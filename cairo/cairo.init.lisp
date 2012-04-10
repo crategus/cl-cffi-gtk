c77eb631fb53c945b06255224915962948975cc5
crategus <crategus@users.sourceforge.net>
2012-04-10 14:52:16 +0200

    Adding the file cairo.init.lisp

---
+++ cairo/cairo.init.lisp
@@ -0,0 +1,37 @@
+;;; ----------------------------------------------------------------------------
+;;; cairo.init.lisp
+;;;
+;;; Copyright (C) 2012 Dieter Kaiser
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License for Lisp
+;;; as published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version and with a preamble to
+;;; the GNU Lesser General Public License that clarifies the terms for use
+;;; with Lisp programs and is referred as the LLGPL.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with this program and the preamble to the Gnu Lesser
+;;; General Public License.  If not, see <http://www.gnu.org/licenses/>
+;;; and <http://opensource.franz.com/preamble.html>.
+;;; ----------------------------------------------------------------------------
+
+(in-package :cairo)
+
+(at-init ()
+  (eval-when (:compile-toplevel :load-toplevel :execute)
+    (define-foreign-library cairo
+      ((:and :unix (:not :darwin))
+       (:or "libcairo.so.2" "libcairo.so"))
+      (:darwin "libcairo.dylib")
+      (:windows "libcariro-2.dll")
+      (t (:default "libcairo"))))
+
+  (use-foreign-library cairo))
+
+;;; --- End of file cairo.init.lisp --------------------------------------------
