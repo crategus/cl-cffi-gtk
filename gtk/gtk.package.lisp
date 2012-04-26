dce97fc6bdfa6e44774d8c50b0c9e5026da21fee
crategus <crategus@users.sourceforge.net>
2012-04-26 22:10:12 +0200

    Adding the library GIO

--- gtk/gtk.package.lisp
+++ gtk/gtk.package.lisp
@@ -26,7 +26,7 @@
 ;;; ----------------------------------------------------------------------------
 
 (defpackage :gtk
-  (:use :cl :cl-user :cffi :gobject :gdk :glib :iter :pango :cairo)
+  (:use :cl :cl-user :cffi :gobject :gdk :glib :gio :iter :pango :cairo)
   (:export #:cl-cffi-gtk-build-info
            
            #:define-child-property
