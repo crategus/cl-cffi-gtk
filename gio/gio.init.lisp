5d0f0ccb0a126a31dd082fc52c38ecf5a6d68cef
crategus <crategus@users.sourceforge.net>
2012-04-26 22:06:39 +0200

    Adding the files gio.init.lisp and gio.package.lisp

---
+++ gio/gio.init.lisp
@@ -0,0 +1,40 @@
+;;; ----------------------------------------------------------------------------
+;;; gio.init.lisp
+;;;
+;;; This file contains code from a fork of cl-gtk2.
+;;; See http://common-lisp.net/project/cl-gtk2/
+;;;
+;;; Copyright (C) 2012 Dieter Kaiser
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License for Lisp
+;;; as published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version and with a preamble to
+;;; the GNU Lesser General Public License that clarifies the terms for use
+;;; with Lisp programs and is referred as the LLGPL.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with this program and the preamble to the Gnu Lesser
+;;; General Public License.  If not, see <http://www.gnu.org/licenses/>
+;;; and <http://opensource.franz.com/preamble.html>.
+;;; ----------------------------------------------------------------------------
+
+(in-package :gio)
+
+(at-init ()
+  (eval-when (:compile-toplevel :load-toplevel :execute)
+    (define-foreign-library glib
+      ((:and :unix (:not :darwin))
+       (:or "libgio-2.0.so.0" "libgio-2.0.so"))
+      (:darwin "libgio-2.0.dylib")
+      (:windows "libgio-2.0.dll")
+      (t (:default "libgio-2.0"))))
+  
+  (use-foreign-library glib))
+
+;;; --- End of file gio.init.lisp ----------------------------------------------
