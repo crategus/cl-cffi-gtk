389fa4bac7c0bb99646491a5734da79c4a04703c
Ralph Schleicher <rs@ralph-schleicher.de>
2017-01-13 18:22:59 +0100

    Fix package scope.

--- gobject/gobject.init.lisp
+++ gobject/gobject.init.lisp
@@ -25,23 +25,18 @@
 ;;; and <http://opensource.franz.com/preamble.html>.
 ;;; ----------------------------------------------------------------------------
 
-(defpackage :cl-gtk2-init
-  (:use :cl :glib))
-
-(in-package :cl-gtk2-init)
+(in-package :gobject)
 
 (glib::at-init ()
  (eval-when (:compile-toplevel :load-toplevel :execute)
-   (cffi:define-foreign-library gobject
+   (define-foreign-library gobject
      ((:and :unix (:not :darwin))
       (:or "libgobject-2.0.so.0" "libgobject-2.0.so"))
      (:darwin (:or "libgobject-2.0.0.dylib" "libgobject-2.0.dylib"))
      (:windows "libgobject-2.0-0.dll")
      (t "libgobject-2.0")))
 
- (cffi:use-foreign-library gobject))
-
-(in-package :gobject)
+ (use-foreign-library gobject))
 
 (defvar *lisp-name-package* nil)
 (defvar *generated-types* nil)
