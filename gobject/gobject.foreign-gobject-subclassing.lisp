3c837cbf6bfafefde7a7e053f429bc03ffaabe19
Olof-Joachim Frahm <olof@macrolet.net>
2017-04-10 22:43:35 +0200

    Remove unnecessary initialisation.
    I'm pretty sure this isn't necessary.  Let's see.

--- gobject/gobject.foreign-gobject-subclassing.lisp
+++ gobject/gobject.foreign-gobject-subclassing.lisp
@@ -463,17 +463,6 @@
                                                             :instance-size)
                                         (callback c-instance-init) nil))
        (add-interfaces ,name))
-     (defmethod initialize-instance :before ((object ,class) &key pointer)
-       (log-for :subclass
-                "(initialize-instance ~A :pointer ~A) :before~%"
-                object pointer)
-       (unless (or pointer
-                   (and (slot-boundp object 'pointer)
-                        (pointer object)))
-         (log-for :subclass "calling g-object-constructor~%")
-         (setf (pointer object)
-               (call-gobject-constructor ,name nil nil)
-               (g-object-has-reference object) t)))
      (progn
        ,@(iter (for (prop-name prop-type prop-accessor prop-reader prop-writer)
                     in properties)
