81806b3859622917e676da6d6830a8c5d78c1c25
Olof-Joachim Frahm <olof@macrolet.net>
2018-11-29 15:44:45 +0100

    Guard other interface for GIO 2.44.

--- gio/gio.list-store.lisp
+++ gio/gio.list-store.lisp
@@ -42,6 +42,7 @@
 ;;; struct GListStore
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (define-g-object-class "GListStore" g-list-store
   (:superclass g-object
    :export t
@@ -55,28 +56,34 @@
 ;;; g_list_store_new ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (declaim (inline g-list-store-new))
 
+#+glib-2-44
 (defun g-list-store-new (item-type)
   (make-instance 'g-list-store :item-type item-type))
 
+#+glib-2-44
 (export 'g-list-store-new)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_insert ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcfun g-list-store-insert :void
   (store (g-object g-list-store))
   (position :uint)
   (item g-object))
 
+#+glib-2-44
 (export 'g-list-store-insert)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_insert_sorted ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcallback g-compare-data-func-callback :int
     ((a g-object)
      (b g-object)
@@ -85,51 +92,61 @@
       (funcall (glib::get-stable-pointer-value data) a b)
     (return () -1)))
 
+#+glib-2-44
 (defcfun ("g_list_store_insert_sorted" %g-list-store-insert-sorted) :uint
   (store (g-object g-list-store))
   (item g-object)
   (func :pointer)
   (data :pointer))
 
+#+glib-2-44
 (defun g-list-store-insert-sorted (store item func)
   (with-stable-pointer (ptr func)
     (%g-list-store-insert-sorted store item (callback g-compare-data-func-callback) ptr)))
 
+#+glib-2-44
 (export 'g-list-store-insert-sorted)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_append ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcfun g-list-store-append :void
   (store (g-object g-list-store))
   (item g-object))
 
+#+glib-2-44
 (export 'g-list-store-append)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_remove ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcfun g-list-store-remove :void
   (store (g-object g-list-store))
   (position :uint))
 
+#+glib-2-44
 (export 'g-list-store-remove)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_remove_all ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcfun g-list-store-remove-all :void
   (store (g-object g-list-store)))
 
+#+glib-2-44
 (export 'g-list-store-remove-all)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_splice ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcfun ("g_list_store_splice" %g-list-store-splice) :void
   (store (g-object g-list-store))
   (position :uint)
@@ -137,6 +154,7 @@
   (additions :pointer)
   (n-additions :uint))
 
+#+glib-2-44
 (defun g-list-store-splice (store position n-removals additions n-additions)
   (with-foreign-objects ((items :pointer n-additions))
     (iterate
@@ -145,19 +163,23 @@
       (setf (mem-aref items :pointer i) (pointer item)))
     (%g-list-store-splice store position n-removals items n-additions)))
 
+#+glib-2-44
 (export 'g-list-store-splice)
 
 ;;; ----------------------------------------------------------------------------
 ;;; g_list_store_sort ()
 ;;; ----------------------------------------------------------------------------
 
+#+glib-2-44
 (defcfun ("g_list_store_sort" %g-list-store-sort) :void
   (store (g-object g-list-store))
   (func :pointer)
   (data :pointer))
 
+#+glib-2-44
 (defun g-list-store-sort (store func)
   (with-stable-pointer (ptr func)
     (%g-list-store-sort store (callback g-compare-data-func-callback) ptr)))
 
+#+glib-2-44
 (export 'g-list-store-sort)
