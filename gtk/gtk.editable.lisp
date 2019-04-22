a6d920e816eb5f89af747615a2308368e208910f
crategus <crategus@users.sourceforge.net>
2019-04-22 12:12:19 +0200

    Update to GTK 3.24

--- gtk/gtk.editable.lisp
+++ gtk/gtk.editable.lisp
@@ -1,15 +1,13 @@
 ;;; ----------------------------------------------------------------------------
 ;;; gtk.editable.lisp
 ;;;
-;;; This file contains code from a fork of cl-gtk2.
-;;; See <http://common-lisp.net/project/cl-gtk2/>.
-;;;
-;;; The documentation has been copied from the GTK+ 3 Reference Manual
-;;; Version 3.16. See <http://www.gtk.org>. The API documentation of the
-;;; Lisp Binding is available at <http://www.crategus.com/books/cl-cffi-gtk/>.
+;;; The documentation of this file is taken from the GTK+ 3 Reference Manual
+;;; Version 3.24 and modified to document the Lisp binding to the GTK library.
+;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
+;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
 ;;; Copyright (C) 2009 - 2011 Kalyanov Dmitry
-;;; Copyright (C) 2011 - 2016 Dieter Kaiser
+;;; Copyright (C) 2011 - 2019 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -55,9 +53,9 @@
 ;;;
 ;;; Signals
 ;;;
-;;;     changed         Run Last
-;;;     delete-text     Run Last
-;;;     insert-text     Run Last
+;;;     void   changed        Run Last
+;;;     void   delete-text    Run Last
+;;;     void   insert-text    Run Last
 ;;;
 ;;; Object Hierarchy
 ;;;
@@ -66,7 +64,7 @@
 ;;;
 ;;; Known Implementations
 ;;;
-;;; GtkEditable is implemented by GtkEntry, GtkSearchEntry and GtkSpinButton.
+;;;     GtkEditable is implemented by GtkEntry, GtkSearchEntry and GtkSpinButton
 ;;; ----------------------------------------------------------------------------
 
 (in-package :gtk)
