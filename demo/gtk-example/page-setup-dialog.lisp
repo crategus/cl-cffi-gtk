f87f1ab053b3e415d11803e8ab47cdbc35006c26
crategus <crategus@users.sourceforge.net>
2019-05-03 00:58:16 +0200

    Add an example for an page setup dialog

---
+++ demo/gtk-example/page-setup-dialog.lisp
@@ -0,0 +1,23 @@
+
+
+(defun example-page-setup-dialog ()
+  (let ((response))
+    (within-main-loop
+      (let ((dialog (gtk-page-setup-unix-dialog-new "Example Page Setup Dialog"
+                                                    nil)))
+        ;; Signal handler for the dialog to handle the signal "destroy".
+        (g-signal-connect dialog "destroy"
+                          (lambda (widget)
+                            (declare (ignore widget))
+                            (leave-gtk-main)))
+        ;; Signal handler for the dialog to handle the signal "response".
+        (g-signal-connect dialog "response"
+                          (lambda (dialog response-id)
+                            (setf response response-id)
+                            (gtk-widget-destroy dialog)))
+        ;; Set a size for the window.
+        (setf (gtk-window-default-size dialog) '(350 200))
+        ;; Show the dialog
+        (gtk-widget-show-all dialog)))
+    (join-gtk-main)
+    (format t "Back from message dialog with response-id : ~A~%" response)))
