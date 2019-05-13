d09e5fdd6db68e0c028ec31da5ec9c5321f341c8
crategus <crategus@users.sourceforge.net>
2019-05-13 23:49:25 +0200

    Work on GtkFileChooserWidget

--- gtk/gtk.file-chooser-widget.lisp
+++ gtk/gtk.file-chooser-widget.lisp
@@ -84,77 +84,121 @@
   (register-object-type "GtkFileChooserWidget" 'gtk-file-chooser-widget))
 
 (define-g-object-class "GtkFileChooserWidget" gtk-file-chooser-widget
-  (:superclass gtk-vbox
+  (:superclass gtk-box
    :export t
    :interfaces ("AtkImplementorIface"
                 "GtkBuildable"
                 "GtkOrientable"
                 "GtkFileChooser"
-;                "GtkFileChooserEmbed" 
+;                TODO: This interface is not documented.
+;                We have no implementation.
+;                "GtkFileChooserEmbed"
                )
    :type-initializer "gtk_file_chooser_widget_get_type")
-  nil)
+  ((search-mode
+    gtk-file-chooser-widget-search-mode
+    "search-mode" "gboolean" t t)
+   (subtitle
+    gtk-file-chooser-widget-subtitle
+    "subtitle" "gchararray" t nil)))
 
 #+cl-cffi-gtk-documentation
 (setf (documentation 'gtk-file-chooser-widget 'type)
- "@version{2013-6-18}
+ "@version{2019-5-13}
   @begin{short}
-    @sym{gtk-file-chooser-widget} is a widget suitable for selecting files. It
-    is the main building block of a @class{gtk-file-chooser-dialog}. Most
-    applications will only need to use the latter; you can use
-    @class{gtk-file-chooser-widget} as part of a larger window if you have
-    special needs.
+    @sym{gtk-file-chooser-widget} is a widget for choosing files.
   @end{short}
-
-  Note that @sym{gtk-file-chooser-widget} does not have any methods of its own.
-  Instead, you should use the functions that work on a @class{gtk-file-chooser}
-  object.")
+  It exposes the @class{gtk-file-chooser} interface, and you should use the
+  methods of this interface to interact with the widget.
+  @begin[CSS nodes]{dictionary}
+    @sym{gtk-file-chooser-widget} has a single CSS node with name
+    @code{filechooser}.
+  @end{dictionary}
+  @see-class{gtk-file-chooser}")
 
 ;;; ----------------------------------------------------------------------------
-;;;
+;;; Property and Accessor Details
+;;; ----------------------------------------------------------------------------
+
+;;; --- gtk-file-chooser-widget-search-mode ------------------------------------
+
+#+cl-cffi-gtk-documentation
+(setf (documentation (atdoc:get-slot-from-name "search-mode"
+                                               'gtk-file-chooser-widget) 't)
+ "The @code{search-mode} property of type @code{:boolean} (Read / Write) @br{}
+  Search mode. @br{}
+  Default value: @code{nil}")
+
+#+cl-cffi-gtk-documentation
+(setf (gethash 'gtk-file-chooser-widget-search-mode atdoc:*function-name-alias*)
+      "Accessor"
+      (documentation 'gtk-file-chooser-widget-search-mode 'function)
+ "@version{2019-5-13}
+  @begin{short}
+    Accessor of the @slot[gtk-file-chooser-widget]{search-mode} slot of the
+    @class{gtk-file-chooser-widget} class.
+  @end{short}
+  @see-class{gtk-file-chooser-widget}")
+
+;;; --- gtk-file-chooser-widget-subtitle ---------------------------------------
+
+#+cl-cffi-gtk-documentation
+(setf (documentation (atdoc:get-slot-from-name "subtitle"
+                                               'gtk-file-chooser-widget) 't)
+ "The @code{subtitle} property of type @code{:string} (Read) @br{}
+  Subtitle. @br{}
+  Default value: \"\"")
+
+#+cl-cffi-gtk-documentation
+(setf (gethash 'gtk-file-chooser-widget-subtitle atdoc:*function-name-alias*)
+      "Accessor"
+      (documentation 'gtk-file-chooser-widget-subtitle 'function)
+ "@version{2019-5-13}
+  @begin{short}
+    Accessor of the @slot[gtk-file-chooser-widget]{subtitle} slot of the
+    @class{gtk-file-chooser-widget} class.
+  @end{short}
+  @see-class{gtk-file-chooser-widget}")
+
+;;; ----------------------------------------------------------------------------
 ;;; Accessors of Child Properties
-;;;
 ;;; ----------------------------------------------------------------------------
+
+;;; --- gtk-file-chooser-widget-child-expand -----------------------------------
 
 (define-child-property "GtkFileChooserWidget"
                        gtk-file-chooser-widget-child-expand
                        "expand" "gboolean" t t t)
-
-(define-child-property "GtkFileChooserWidget"
-                       gtk-file-chooser-widget-child-fill
-                       "fill" "gboolean" t t t)
-
-(define-child-property "GtkFileChooserWidget"
-                       gtk-file-chooser-widget-child-padding
-                       "padding" "guint" t t t)
-
-(define-child-property "GtkFileChooserWidget"
-                       gtk-file-chooser-widget-child-pack-type
-                       "pack-type" "GtkPackType" t t t)
-
-(define-child-property "GtkFileChooserWidget"
-                       gtk-file-chooser-widget-child-position
-                       "position" "gint" t t t)
-
-;;; ----------------------------------------------------------------------------
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gtk-file-chooser-widget-child-expand atdoc:*function-name-alias*)
       "Accessor"
       (documentation 'gtk-file-chooser-widget-child-expand 'function)
  "@version{2013-8-27}
-  Accessor of the child property @code{\"expand\"} of the
+  Accessor of the @code{expand} child property of the
   @class{gtk-file-chooser-widget} class.
   @see-class{gtk-file-chooser-widget}")
+
+;;; --- gtk-file-chooser-widget-child-fill -------------------------------------
+
+(define-child-property "GtkFileChooserWidget"
+                       gtk-file-chooser-widget-child-fill
+                       "fill" "gboolean" t t t)
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gtk-file-chooser-widget-child-fill atdoc:*function-name-alias*)
       "Accessor"
       (documentation 'gtk-file-chooser-widget-child-fill 'function)
  "@version{2013-8-27}
-  Accessor of the child property @code{\"fill\"} of the
+  Accessor of the @code{fill} child property of the
   @class{gtk-file-chooser-widget} class.
   @see-class{gtk-file-chooser-widget}")
+
+;;; --- gtk-file-chooser-widget-child-padding ----------------------------------
+
+(define-child-property "GtkFileChooserWidget"
+                       gtk-file-chooser-widget-child-padding
+                       "padding" "guint" t t t)
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gtk-file-chooser-widget-child-padding
@@ -162,9 +206,15 @@
       "Accessor"
       (documentation 'gtk-file-chooser-widget-child-padding 'function)
  "@version{2013-8-27}
-  Accessor of the child property @code{\"padding\"} of the
+  Accessor of the @code{padding} child property of the
   @class{gtk-file-chooser-widget} class.
   @see-class{gtk-file-chooser-widget}")
+
+;;; --- gtk-file-chooser-widget-child-pack-type --------------------------------
+
+(define-child-property "GtkFileChooserWidget"
+                       gtk-file-chooser-widget-child-pack-type
+                       "pack-type" "GtkPackType" t t t)
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gtk-file-chooser-widget-child-pack-type
@@ -172,9 +222,15 @@
       "Accessor"
       (documentation 'gtk-file-chooser-widget-child-pack-type 'function)
  "@version{2013-8-27}
-  Accessor of the child property @code{\"pack-type\"} of the
+  Accessor of the @code{pack-type} child property of the
   @class{gtk-file-chooser-widget} class.
   @see-class{gtk-file-chooser-widget}")
+
+;;; --- gtk-file-chooser-widget-child-position ---------------------------------
+
+(define-child-property "GtkFileChooserWidget"
+                       gtk-file-chooser-widget-child-position
+                       "position" "gint" t t t)
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gtk-file-chooser-widget-child-position
@@ -182,7 +238,7 @@
       "Accessor"
       (documentation 'gtk-file-chooser-widget-child-position 'function)
  "@version{2013-8-27}
-  Accessor of the child property @code{\"position\"} of the
+  Accessor of the @code{position} child property of the
   @class{gtk-file-chooser-widget} class.
   @see-class{gtk-file-chooser-widget}")
 
@@ -195,7 +251,7 @@
 (defun gtk-file-chooser-widget-new (action)
  #+cl-cffi-gtk-documentation
  "@version{2013-8-27}
-  @argument[action]{open or save mode for the widget, a value of the 
+  @argument[action]{open or save mode for the widget, a value of the
     @symbol{gtk-file-chooser-action} enumeration}
   @return{A new @class{gtk-file-chooser-widget} widget.}
   @begin{short}
@@ -203,8 +259,6 @@
   @end{short}
   This is a file chooser widget that can be embedded in custom windows, and it
   is the same widget that is used by @class{gtk-file-chooser-dialog}.
-
-  Since 2.4
   @see-class{gtk-file-chooser-widget}
   @see-class{gtk-file-chooser-dialog}
   @see-symbol{gtk-file-chooser-action}"
