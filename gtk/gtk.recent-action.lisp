c53ef68ed33f599f8a918f0a0ae8804f85455f79
crategus <crategus@users.sourceforge.net>
2020-01-19 15:53:04 +0100

    Update the documentation to GTK+ 3.24

--- gtk/gtk.recent-action.lisp
+++ gtk/gtk.recent-action.lisp
@@ -1,16 +1,13 @@
 ;;; ----------------------------------------------------------------------------
 ;;; gtk.recent-action.lisp
 ;;;
-;;; This file contains code from a fork of cl-gtk2.
-;;; See <http://common-lisp.net/project/cl-gtk2/>.
-;;;
 ;;; The documentation of this file is taken from the GTK+ 3 Reference Manual
-;;; Version 3.8.8 and modified to document the Lisp binding to the GTK library.
+;;; Version 3.24 and modified to document the Lisp binding to the GTK library.
 ;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
 ;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
 ;;; Copyright (C) 2009 - 2011 Kalyanov Dmitry
-;;; Copyright (C) 2011 - 2013 Dieter Kaiser
+;;; Copyright (C) 2011 - 2019 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -32,16 +29,32 @@
 ;;;
 ;;; GtkRecentAction
 ;;;
-;;; An action of which represents a list of recently used files
+;;;     An action of which represents a list of recently used files
 ;;;
-;;; Synopsis
+;;; Types and Values
 ;;;
 ;;;     GtkRecentAction
 ;;;
+;;; Functions
+;;;
 ;;;     gtk_recent_action_new
 ;;;     gtk_recent_action_new_for_manager
-;;;     gtk_recent_action_get_show_numbers
-;;;     gtk_recent_action_set_show_numbers
+;;;     gtk_recent_action_get_show_numbers                 Accessor
+;;;     gtk_recent_action_set_show_numbers                 Accessor
+;;;
+;;; Properties
+;;;
+;;;     gboolean   show-numbers    Read / Write
+;;;
+;;; Object Hierarchy
+;;;
+;;;     GObject
+;;;     ╰── GtkAction
+;;;         ╰── GtkRecentAction
+;;;
+;;; Implemented Interfaces
+;;;
+;;;     GtkRecentAction implements GtkBuildable and GtkRecentChooser.
 ;;; ----------------------------------------------------------------------------
 
 (in-package :gtk)
@@ -72,7 +85,8 @@
   To construct a submenu showing recently used files, use a
   @sym{gtk-recent-action} as the action for a <menuitem>. To construct a menu
   toolbutton showing the recently used files in the popup menu, use a
-  @sym{gtk-recent-action} as the action for a <toolitem> element.")
+  @sym{gtk-recent-action} as the action for a <toolitem> element.
+  @see-slot{gtk-recent-action-show-numbers}")
 
 ;;; ----------------------------------------------------------------------------
 ;;; Property and Accessor Details
@@ -90,42 +104,44 @@
 (setf (gethash 'gtk-recent-action-show-numbers atdoc:*function-name-alias*)
       "Accessor"
       (documentation 'gtk-recent-action-show-numbers 'function)
- "@version{2013-6-2}
-  Accessor of the @slot[gtk-recent-action]{show-numbers} slot of the
-  @class{gtk-recent-action} class.
+ "@version{2019-6-6}
+  @argument[object]{a @class{gtk-recent-action} object}
+  @argument[show-numbers]{@em{true} if the shown items should be numbered}
+  @begin{short}
+    Accessor of the @slot[gtk-recent-action]{show-numbers} slot of the
+    @class{gtk-recent-action} class.
+  @end{short}
+
+  The @sym{gtk-recent-action-show-numbers} slot access function
+  sets whether a number should be added to the items shown by the widgets
+  representing action. The numbers are shown to provide a unique character for
+  a mnemonic to be used inside the menu item's label. Only the first ten items
+  get a number to avoid clashes.
   @see-class{gtk-recent-action}")
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_recent_action_new ()
-;;;
-;;; GtkAction * gtk_recent_action_new (const gchar *name,
-;;;                                    const gchar *label,
-;;;                                    const gchar *tooltip,
-;;;                                    const gchar *stock_id);
-;;;
-;;; Creates a new GtkRecentAction object. To add the action to a GtkActionGroup
-;;; and set the accelerator for the action, call
-;;; gtk_action_group_add_action_with_accel().
-;;;
-;;; name :
-;;;     a unique name for the action
-;;;
-;;; label :
-;;;     the label displayed in menu items and on buttons, or NULL
-;;;
-;;; tooltip :
-;;;     a tooltip for the action, or NULL
-;;;
-;;; stock_id :
-;;;     the stock icon to display in widgets representing the action, or NULL
-;;;
-;;; Returns :
-;;;     the newly created GtkRecentAction.
-;;;
-;;; Since 2.12
 ;;; ----------------------------------------------------------------------------
 
 (defcfun ("gtk_recent_action_new" gtk-recent-action-new) (g-object gtk-action)
+ "@version{2019-6-6}
+  @argument[name]{a unique name for the action}
+  @argument[label]{the label displayed in menu items and on buttons, or
+    @code{nil}}
+  @argument[tooltip]{a tooltip for the action, or @code{nil}}
+  @argument[stock-id]{the stock icon to display in widgets representing the
+    action, or @code{nil}}
+  @return{The newly created @class{gtk-recent-action}.}
+  @begin{short}
+    Creates a new @class{gtk-recent-action} object.
+  @end{short}
+  To add the action to a @class{gtk-action-group} and set the accelerator for
+  the action, call the @fun{gtk-action-group-add-action} function.
+  @begin[Warning]{dictionary}
+    The @sym{gtk-recent-action-new} function has been deprecated since version
+    3.10 and should not be used in newly-written code.
+  @end{dictionary}
+  @see-class{gtk-recent-action}"
   (name :string)
   (label :string)
   (tooltip :string)
@@ -135,40 +151,30 @@
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_recent_action_new_for_manager ()
-;;;
-;;; GtkAction * gtk_recent_action_new_for_manager (const gchar *name,
-;;;                                                const gchar *label,
-;;;                                                const gchar *tooltip,
-;;;                                                const gchar *stock_id,
-;;;                                                GtkRecentManager *manager);
-;;;
-;;; Creates a new GtkRecentAction object. To add the action to a GtkActionGroup
-;;; and set the accelerator for the action, call
-;;; gtk_action_group_add_action_with_accel().
-;;;
-;;; name :
-;;;     a unique name for the action
-;;;
-;;; label :
-;;;     the label displayed in menu items and on buttons, or NULL
-;;;
-;;; tooltip :
-;;;     a tooltip for the action, or NULL
-;;;
-;;; stock_id :
-;;;     the stock icon to display in widgets representing the action, or NULL
-;;;
-;;; manager :
-;;;     a GtkRecentManager, or NULL for using the default GtkRecentManager
-;;;
-;;; Returns :
-;;;     the newly created GtkRecentAction
-;;;
-;;; Since 2.12
 ;;; ----------------------------------------------------------------------------
 
 (defcfun ("gtk_recent_action_new_for_manager"
            gtk-recent-action-new-for-manager) (g-object gtk-action)
+ "@version{2019-6-6}
+  @argument[name]{a unique name for the action}
+  @argument[label]{the label displayed in menu items and on buttons, or
+    @code{nil}}
+  @argument[tooltip]{a tooltip for the action, or @code{nil}}
+  @argument[stock-id]{the stock icon to display in widgets representing the
+    action, or @code{nil}}
+  @argument[manager]{a @class{gtk-recent-manager}, or @code{nil} for using the
+    default @class{gtk-recent-manager}}
+  @return{The newly created @class{gtk-recent-action}.}
+  @begin{short}
+    Creates a new @class{gtk-recent-action} object.
+  @end{short}
+  To add the action to a @class{gtk-action-group} and set the accelerator for
+  the action, call the @fun{gtk-action-group-add-action} function.
+  @begin[Warning]{dictionary}
+    The @sym{gtk-recent-action-new-manager} function has been deprecated since
+    version 3.10 and should not be used in newly-written code.
+  @end{dictionary}
+  @see-class{gtk-recent-action}"
   (name :string)
   (label :string)
   (tooltip :string)
@@ -176,55 +182,5 @@
   (manager (g-object gtk-recent-manager)))
 
 (export 'gtk-recent-action-new-for-manager)
-
-;;; ----------------------------------------------------------------------------
-;;; gtk_recent_action_get_show_numbers ()
-;;;
-;;; gboolean gtk_recent_action_get_show_numbers (GtkRecentAction *action);
-;;;
-;;; Returns the value set by gtk_recent_chooser_menu_set_show_numbers().
-;;;
-;;; action :
-;;;     a GtkRecentAction
-;;;
-;;; Returns :
-;;;     TRUE if numbers should be shown.
-;;;
-;;; Since 2.12
-;;; ----------------------------------------------------------------------------
-
-(declaim (inline gtk-recent-action-get-show-numbers))
-
-(defun gtk-recent-action-get-show-numbers (action)
-  (gtk-recent-action-show-numbers action))
-
-(export 'gtk-recent-action-get-show-numbers)
-
-;;; ----------------------------------------------------------------------------
-;;; gtk_recent_action_set_show_numbers ()
-;;;
-;;; void gtk_recent_action_set_show_numbers (GtkRecentAction *action,
-;;;                                          gboolean show_numbers);
-;;;
-;;; Sets whether a number should be added to the items shown by the widgets
-;;; representing action. The numbers are shown to provide a unique character for
-;;; a mnemonic to be used inside the menu item's label. Only the first ten items
-;;; get a number to avoid clashes.
-;;;
-;;; action :
-;;;     a GtkRecentAction
-;;;
-;;; show_numbers :
-;;;     TRUE if the shown items should be numbered
-;;;
-;;; Since 2.12
-;;; ----------------------------------------------------------------------------
-
-(declaim (inline gtk-recent-action-set-show-numbers))
-
-(defun gtk-recent-action-set-show-numbers (action show-numbers)
-  (setf (gtk-recent-action-show-numbers action) show-numbers))
-
-(export 'gtk-recent-action-set-show-numbers)
 
 ;;; --- End of file gtk.recent-action.lisp -------------------------------------
