0a7070541011c98a628c3c1e3a1c87b72ad2fe0a
crategus <crategus@users.sourceforge.net>
2020-01-26 22:48:56 +0100

    Correcting a typo

--- gtk/gtk.widget.lisp
+++ gtk/gtk.widget.lisp
@@ -2,12 +2,12 @@
 ;;; gtk.widget.lisp
 ;;;
 ;;; The documentation of this file is taken from the GTK+ 3 Reference Manual
-;;; Version 3.24 and modified to document the Lisp binding to the GTK library.
+;;; Version 3.24 and modified to document the Lisp binding to the GTK+ library.
 ;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
 ;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
 ;;; Copyright (C) 2009 - 2011 Kalyanov Dmitry
-;;; Copyright (C) 2011 - 2019 Dieter Kaiser
+;;; Copyright (C) 2011 - 2020 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -29,7 +29,7 @@
 ;;;
 ;;; GtkWidget
 ;;;
-;;; Base class for all widgets
+;;;     Base class for all widgets
 ;;;
 ;;; Types and Values
 ;;;
@@ -2418,7 +2418,7 @@
   Whether the widget should grab focus when it is clicked with the mouse.
   This property is only relevant for widgets that can take focus.
   Before 3.20, several widgets (@class{gtk-button},
-  @class{gtk-file-chosser-button}, @class{gtk-combo-box}) implemented this
+  @class{gtk-file-chooser-button}, @class{gtk-combo-box}) implemented this
   property individually. @br{}
   Default value: @em{true} @br{}
   Since 3.20")
