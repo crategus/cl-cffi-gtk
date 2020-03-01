99c6e17a4844eaa61c0660b49ae2625444f660a0
crategus <crategus@users.sourceforge.net>
2020-03-02 00:42:03 +0100

    Add an example for GApplication

--- demo/gtk-example/simple-application.lisp
+++ demo/gtk-example/simple-application.lisp
@@ -31,3 +31,34 @@
 
       ;; Run the application
       (g-application-run app argv))))
+
+(defun simple-application2 (&optional (argv nil))
+  (within-main-loop
+    (let (;; Create an application
+          (app (make-instance 'gtk-application
+                              :application-id "com.crategus.simple-application"
+                              :flags :none)))
+
+      ;; Connect signal "activate" to the applicaton
+      (g-signal-connect app "activate"
+          (lambda (application)
+            ;; Create an application window
+            (let ((window (make-instance 'gtk-application-window
+                                         :application application
+                                         :title "Simple Application"
+                                         :default-width 500
+                                         :default-height 300)))
+              ;; Connect signal "destroy" to the application window
+              (g-signal-connect window "destroy"
+                                (lambda (widget)
+                                  (declare (ignore widget))
+                                  ;; Leave the main loop
+                                  (leave-gtk-main)
+                                  ;; Quit the application
+                                  (g-application-quit app)))
+              ;; Show the application window
+              (gtk-widget-show-all window))))
+
+      ;; Run the application
+      (g-application-run app argv)))
+      (join-gtk-main))
