a7ed05776bbdde4f32013bb79b9204bff56eee80
crategus <crategus@users.sourceforge.net>
2020-03-21 14:00:06 +0100

    Work on the implementation of GtkTextMark

--- gtk/gtk.text-mark.lisp
+++ gtk/gtk.text-mark.lisp
@@ -2,12 +2,12 @@
 ;;; gtk.text-mark.lisp
 ;;;
 ;;; The documentation of this file is taken from the GTK+ 3 Reference Manual
-;;; Version 3.24 and modified to document the Lisp binding to the GTK library.
+;;; Version 3.24 and modified to document the Lisp binding to the GTK+ library.
 ;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
 ;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
 ;;; Copyright (C) 2009 - 2011 Kalyanov Dmitry
-;;; Copyright (C) 2011 - 2019 Dieter Kaiser
+;;; Copyright (C) 2011 - 2020 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -118,16 +118,16 @@
  "The @code{left-gravity} property of type @code{:boolean}
   (Read / Write / Construct) @br{}
   Whether the mark has left gravity. @br{}
-  Default value: @code{nil}")
+  Default value: @em{false}")
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gtk-text-mark-left-gravity atdoc:*function-name-alias*)
       "Accessor"
       (documentation 'gtk-text-mark-left-gravity 'function)
- "@version{2016-1-30}
+ "@version{2020-3-14}
   @syntax[]{(gtk-text-mark-left-gravity object) => gravity}
   @argument[object]{a @class{gtk-text-mark} object}
-  @return{@emph{True} if the text mark has left gravity.}
+  @return{@em{True} if the text mark has left gravity.}
   @begin{short}
     Accessor of the @slot[gtk-text-mark]{left-gravity} slot of the
     @class{gtk-text-mark} class.
@@ -150,14 +150,17 @@
 (setf (gethash 'gtk-text-mark-name atdoc:*function-name-alias*)
       "Accessor"
       (documentation 'gtk-text-mark-name 'function)
- "@version{2016-1-30}
+ "@version{2020-3-14}
+  @syntax[]{(gtk-text-mark-name object) => mark-name}
+  @argument[object]{a @class{gtk-text-mark} object}
+  @argument[mark-name]{a string with the mark name}
   @begin{short}
     Accessor of the @slot[gtk-text-mark]{name} slot of the
     @class{gtk-text-mark} class.
   @end{short}
 
-  The @sym{gtk-text-mark-name} slot access function returns the name of the text
-  mark or @code{nil} for anonymous marks.
+  The @sym{gtk-text-mark-name} slot access function returns the name of the
+  text mark or @code{nil} for anonymous marks.
   @see-class{gtk-text-mark}")
 
 ;;; ----------------------------------------------------------------------------
@@ -167,24 +170,25 @@
 (defun gtk-text-mark-new (name left-gravity)
  #+cl-cffi-gtk-documentation
  "@version{2013-8-13}
-  @argument[name]{mark name or @code{nil}}
-  @argument[left-gravity]{whether the mark should have left gravity}
+  @argument[name]{a string with the mark name or @code{nil}}
+  @argument[left-gravity]{a @code{:boolean} whether the mark should have left
+    gravity}
   @return{New @class{gtk-text-mark} object.}
   @begin{short}
     Creates a text mark.
   @end{short}
   Add it to a buffer using the function @fun{gtk-text-buffer-add-mark}. If
   @arg{name} is @code{nil}, the mark is anonymous; otherwise, the mark can be
-  retrieved by @arg{name} using the function @fun{gtk-text-buffer-get-mark}. If
-  a mark has left gravity, and text is inserted at the mark's current location,
-  the mark will be moved to the left of the newly-inserted text. If the mark has
-  right gravity, @arg{left-gravity} = @code{nil}, the mark will end up on the
-  right of newly inserted text. The standard left-to-right cursor is a mark with
-  right gravity, when you type, the cursor stays on the right side of the text
-  you are typing.
+  retrieved by @arg{name} using the function @fun{gtk-text-buffer-mark}. If a
+  mark has left gravity, and text is inserted at the mark's current location,
+  the mark will be moved to the left of the newly-inserted text. If the mark
+  has right gravity, @arg{left-gravity} = @code{nil}, the mark will end up on
+  the right of newly inserted text. The standard left-to-right cursor is a mark
+  with right gravity, when you type, the cursor stays on the right side of the
+  text you are typing.
   @see-class{gtk-text-mark}
-  @see-function{gtk-text-buffer-add-mark}
-  @see-function{gtk-text-buffer-get-mark}"
+  @see-function{gtk-text-buffer-mark}
+  @see-function{gtk-text-buffer-add-mark}"
   (if name
       (make-instance 'gtk-text-mark
                      :name name
@@ -196,79 +200,78 @@
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_text_mark_set_visible ()
+;;; gtk_text_mark_get_visible ()
 ;;; ----------------------------------------------------------------------------
 
-(defcfun ("gtk_text_mark_set_visible" gtk-text-mark-set-visible) :void
+(defun (setf gtk-text-mark-visible) (setting mark)
+  (foreign-funcall "gtk_text_mark_set_visible"
+                   (g-object) mark :boolean setting :void)
+  setting)
+
+(defcfun ("gtk_text_mark_get_visible" gtk-text-mark-visible) :boolean
  #+cl-cffi-gtk-documentation
- "@version{2013-4-29}
+ "@version{2020-3-14}
+  @syntax[]{(gtk-text-mark-visible mark) => visibility}
+  @syntax[]{(setf (gtk-text-mark-visible mark) visibility)}
   @argument[mark]{a @class{gtk-text-mark} object}
-  @argument[setting]{visibility of @arg{mark}}
+  @argument[visibility]{a @code{:boolean} whether the mark is visible}
+  @return{@em{True} if visible.}
   @begin{short}
-    Sets the visibility of @arg{mark}.
+    Accessor for the visibility of a text mark.
   @end{short}
+
+  The function @sym{gtk-text-mark-visible} returns @em{true} if @arg{mark} is
+  visible (i. e. a cursor is displayed for it). The function
+  @sym{(setf gtk-text-mark-visible)} sets the visibility of @arg{mark}.
+
   The insertion point is normally visible, i. e. you can see it as a vertical
   bar. Also, the text widget uses a visible mark to indicate where a drop will
   occur when dragging-and-dropping text. Most other marks are not visible.
   Marks are not visible by default.
   @see-class{gtk-text-mark}"
-  (mark (g-object gtk-text-mark))
-  (setting :boolean))
-
-(export 'gtk-text-mark-set-visible)
-
-;;; ----------------------------------------------------------------------------
-;;; gtk_text_mark_get_visible ()
-;;; ----------------------------------------------------------------------------
-
-(defcfun ("gtk_text_mark_get_visible" gtk-text-mark-get-visible) :boolean
- #+cl-cffi-gtk-documentation
- "@version{2016-1-30}
-  @argument[mark]{a @class{gtk-text-mark} object}
-  @return{@em{True} if visible.}
-  Returns @em{true} if @arg{mark} is visible (i. e. a cursor is displayed
-  for it).
-  @see-class{gtk-text-mark}"
   (mark (g-object gtk-text-mark)))
 
-(export 'gtk-text-mark-get-visible)
+(export 'gtk-text-mark-visible)
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_text_mark_get_deleted ()
 ;;; ----------------------------------------------------------------------------
 
-(defcfun ("gtk_text_mark_get_deleted" gtk-text-mark-get-deleted) :boolean
+(defcfun ("gtk_text_mark_get_deleted" gtk-text-mark-deleted) :boolean
  #+cl-cffi-gtk-documentation
- "@version{2016-1-30}
+ "@version{2020-3-14}
   @argument[mark]{a @class{gtk-text-mark} object}
-  @return{Whether the @arg{mark} is deleted.}
+  @return{A @code{:boolean} whether the mark is deleted.}
   @begin{short}
-    Returns @em{true} if @arg{mark} has been removed from its buffer with the
+    Returns @em{true} if the mark has been removed from its buffer with the
     function @fun{gtk-text-buffer-delete-mark}.
   @end{short}
   See the function @fun{gtk-text-buffer-add-mark} for a way to add it to a
   buffer again.
   @see-class{gtk-text-mark}
-  @see-function{gtk-text-buffer-delete-mark}
-  @see-function{gtk-text-buffer-add-mark}"
+  @see-function{gtk-text-buffer-add-mark}
+  @see-function{gtk-text-buffer-delete-mark}"
   (mark (g-object gtk-text-mark)))
 
-(export 'gtk-text-mark-get-deleted)
+(export 'gtk-text-mark-deleted)
 
 ;;; ----------------------------------------------------------------------------
 ;;; gtk_text_mark_get_buffer ()
 ;;; ----------------------------------------------------------------------------
 
-(defcfun ("gtk_text_mark_get_buffer" gtk-text-mark-get-buffer)
+(defcfun ("gtk_text_mark_get_buffer" gtk-text-mark-buffer)
     (g-object gtk-text-buffer)
  #+cl-cffi-gtk-documentation
- "@version{2016-1-30}
+ "@version{2020-3-14}
   @argument[mark]{a @class{gtk-text-mark} object}
-  @return{The @arg{mark}'s @class{gtk-text-buffer} object.}
-  Gets the buffer this @arg{mark} is located inside, or @code{nil} if the
-  @arg{mark} is deleted.
+  @return{The @class{gtk-text-buffer} object of the mark.}
+  @begin{short}
+    Gets the buffer this mark is located inside, or @code{nil} if the mark is
+    deleted.
+  @end{short}
   @see-class{gtk-text-mark}"
   (mark (g-object gtk-text-mark)))
 
-(export 'gtk-text-mark-get-buffer)
+(export 'gtk-text-mark-buffer)
 
 ;;; --- End of file gtk.text-mark.lisp -----------------------------------------
