7a4ded7908729f11818369260a55d31319753f40
crategus <crategus@users.sourceforge.net>
2020-08-17 16:36:16 +0200

    Adding an example to the documentation

--- gtk/gtk.event-box.lisp
+++ gtk/gtk.event-box.lisp
@@ -84,13 +84,53 @@
 
 #+cl-cffi-gtk-documentation
 (setf (documentation 'gtk-event-box 'type)
- "@version{2020-6-3}
+ "@version{2020-8-17}
   @begin{short}
     The @sym{gtk-event-box} widget is a subclass of @class{gtk-bin} which also
     has its own window.
   @end{short}
   It is useful since it allows you to catch events for widgets which do not
   have their own window.
+  @begin[Example]{dictionary}
+    This example demonstrates the usage of a GtkEventBox widget - a label is
+    created and set up so that a mouse-click on the label causes the program to
+    exit.
+   @begin{pre}
+(defun example-event-box ()
+  (within-main-loop
+    (let ((window (make-instance 'gtk-window
+                                 :type :toplevel
+                                 :title \"Example Event Box\"
+                                 :default-height 150
+                                 :border-width 24))
+          (eventbox (make-instance 'gtk-event-box))
+          (label (make-instance 'gtk-label
+                                :ellipsize :end
+                                :label
+                                \"Click here to quit this Example Event Box.\")))
+      (g-signal-connect window \"destroy\"
+                        (lambda (widget)
+                          (declare (ignore widget))
+                          (leave-gtk-main)))
+      ;; Set the available events for the event box
+      (setf (gtk-widget-events eventbox) :button-press-mask)
+      ;; Connect a signal handler to the eventbox
+      (g-signal-connect eventbox \"button-press-event\"
+                        (lambda (widget event)
+                          (declare (ignore widget event))
+                          (gtk-widget-destroy window)))
+      ;; Add the label to the event box and the event box to the window
+      (gtk-container-add eventbox label)
+      (gtk-container-add window eventbox)
+      ;; Realize the event box
+      (gtk-widget-realize eventbox)
+      ;; Set a new cursor for the event box
+      (setf (gdk-window-cursor (gtk-widget-window eventbox))
+            (gdk-cursor-new-from-name (gdk-display-default) \"pointer\"))
+      ;; Show the window
+      (gtk-widget-show-all window))))
+    @end{pre}
+  @end{dictionary}
   @see-slot{gtk-event-box-above-child}
   @see-slot{gtk-event-box-visible-window}
   @see-class{gtk-bin}")
