4316afc4b8546aa4c9f4042d98aa5d2b17abd1e8
crategus <crategus@users.sourceforge.net>
2020-08-17 16:38:10 +0200

    Updating the example Event Box

--- demo/gtk-demo/event-box.lisp
+++ demo/gtk-demo/event-box.lisp
@@ -5,16 +5,16 @@
 ;;;; on the label causes the program to exit. Resizing the window reveals
 ;;;; varying amounts of the label.
 ;;;;
-;;;; In addition, example Event Box shows how to change the mouse pointer over a
+;;;; In addition, example Event Box shows how to change the cursor over a
 ;;;; window. Every widget has an associated window of type GdkWindow, which can
 ;;;; be get with the function gtk-widget-window. The function gdk-window-cursor
-;;;; sets a mouse pointer for this GdkWindow. A new mouse pointer is created
-;;;; with the function gdk-cursor-new. The functions takes one argument, which
-;;;; is a keyword of the enumeration type GdkCursorType for a predefined mouse
-;;;; pointer. GTK+ has about 150 predefined mouse pointers. Look at the
-;;;; implementation of GdkCursorType in the file gdk.cursor.lisp to see all
-;;;; possible keywords. In example Event Box the mouse pointer :hand1 is chosen.
-;;;; This mouse pointer is than associated to the GdkWindow with the function
+;;;; sets a cursor for this GdkWindow. A new cursor is created with the function
+;;;; gdk-cursor-new-from-name. The function takes two arguments. The first
+;;;; argument is the GdkDisplay object for which the cursor will be created.
+;;;; The second argument is a string with the name of the cursor. Look at the
+;;;; documentation of the function gdk-cursor-new-from-name for available cursor
+;;;; names. In example Event Box the cursor with the name "pointer" is chosen.
+;;;; This cursor is associated to the GdkWindow with the function
 ;;;; gdk-window-cursor.
 
 (in-package :gtk-demo)
@@ -35,9 +35,9 @@
                         (lambda (widget)
                           (declare (ignore widget))
                           (leave-gtk-main)))
-      ;; Set the events for the event box
+      ;; Set the available events for the event box
       (setf (gtk-widget-events eventbox) :button-press-mask)
-      ;; Connect a signal to the eventbox
+      ;; Connect a signal handler to the eventbox
       (g-signal-connect eventbox "button-press-event"
                         (lambda (widget event)
                           (declare (ignore widget event))
@@ -49,7 +49,7 @@
       (gtk-widget-realize eventbox)
       ;; Set a new cursor for the event box
       (setf (gdk-window-cursor (gtk-widget-window eventbox))
-            (gdk-cursor-new :hand1))
+            (gdk-cursor-new-from-name (gdk-display-default) "pointer"))
       ;; Show the window
       (gtk-widget-show-all window))))
 
