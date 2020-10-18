360c6b9d99f72f5fd8ef8efd5586bde953a3fe7d
crategus <crategus@users.sourceforge.net>
2020-10-18 13:29:09 +0200

    Work on the tests for GObject

--- test/rtest-gobject-base.lisp
+++ test/rtest-gobject-base.lisp
@@ -1,103 +1,60 @@
 (def-suite gobject-base :in gobject-suite)
 (in-suite gobject-base)
 
-;;;   GParameter
+;;;     GObject
+;;;     GObjectClass
+;;;     GParameter
+;;;     GObjectConstructParam
+;;;     GInitiallyUnowned
+;;;     GInitiallyUnownedClass
 
-(test g-parameter
-  #-windows
-  (is (= 32 (foreign-type-size '(:struct g-parameter))))
-  #+windows
-  (is (= 32 (foreign-type-size '(:struct g-parameter))))
-  (is (equal '(:name :value)
-             (foreign-slot-names '(:struct g-parameter)))))
+;;;     g-type-is-object
 
-(test g-parameter.1
-  (with-foreign-object (parameter '(:struct g-parameter))
-    (setf (foreign-slot-value parameter '(:struct g-parameter) :name)
-          "name")
-    (set-g-value (foreign-slot-pointer parameter '(:struct g-parameter) :value)
-                 12
-                 "gint"
-                 :zero-g-value t)
-;    (format t "~a~%~a~%"
-;            (foreign-slot-value parameter '(:struct g-parameter) :name)
-;            (foreign-slot-value parameter '(:struct g-parameter) :value))
-    (is (equal "name" (foreign-slot-value parameter '(:struct g-parameter) :name)))
-    (is (= 12 (parse-g-value (foreign-slot-pointer parameter '(:struct g-parameter) :value))))
-  ))
+(test g-type-is-object
+  (is-true  (g-type-is-object "GtkButton"))
+  (is-false (g-type-is-object "gboolean"))
+  (is-false (g-type-is-object "GtkWindowType")))
 
-(test g-parameter.2
-  (with-foreign-object (parameter '(:struct g-parameter))
-    (setf (foreign-slot-value parameter '(:struct g-parameter) :name)
-          "name")
-    (set-g-value (foreign-slot-pointer parameter '(:struct g-parameter) :value)
-                 "text"
-                 "gchararray"
-                 :zero-g-value t)
-;    (format t "~a~%~a~%"
-;            (foreign-slot-value parameter '(:struct g-parameter) :name)
-;            (foreign-slot-value parameter '(:struct g-parameter) :value))
-    (is (equal "name" (foreign-slot-value parameter '(:struct g-parameter) :name)))
-    (is (equal "text" (parse-g-value (foreign-slot-pointer parameter '(:struct g-parameter) :value))))
-  ))
+;;;     g-is-object
 
-(test g-parameter.3
-  (let* ((args-names (list "title" "border-width"))
-         (args-values (list "text" 12))
-         (args-types (list "gchararray" "gint"))
-         (args-count (length args-names)))
-    (with-foreign-object (parameters '(:struct g-parameter) args-count)
-      (loop
-         for i from 0 below args-count
-         for arg-name in args-names
-         for arg-value in args-values
-         for arg-type in args-types
-         for arg-g-type = arg-type
-         for parameter = (mem-aptr parameters '(:struct g-parameter) i)
-         do (setf (foreign-slot-value parameter '(:struct g-parameter) :name)
-                  arg-name)
-         do (set-g-value (foreign-slot-pointer parameter
-                                               '(:struct g-parameter) :value)
-                         arg-value
-                         arg-g-type
-                         :zero-g-value t)
-;        do (format t "(~A, ~A)~%"
-;                   (foreign-slot-value parameter '(:struct g-parameter) :name)
-;                   (parse-g-value (foreign-slot-pointer parameter '(:struct g-parameter) :value)))
-      )
-      (loop
-         for i from 0 below args-count
-         for arg-name in args-names
-         for arg-value in args-values
-         for parameter = (mem-aptr parameters '(:struct g-parameter) i)
-         do (is (equal arg-name (foreign-slot-value parameter '(:struct g-parameter) :name)))
-         do (is (equal arg-value
-                       (parse-g-value (foreign-slot-pointer parameter '(:struct g-parameter) :value))))
-         do (foreign-string-free (mem-ref (foreign-slot-pointer parameter
-                                                                '(:struct g-parameter)
-                                                                :name)
-                                          :pointer))
-         do (g-value-unset (foreign-slot-pointer parameter
-                                                 '(:struct g-parameter)
-                                                 :value))))))
+(test g-is-object
+  (is-true  (g-is-object (make-instance 'gtk-button))))
 
-;;;    GObject
-;;;    GObjectClass
-;;;    GObjectConstructParam
-;;;
-;;;    G_TYPE_IS_OBJECT
-;;;    G_OBJECT
-;;;    G_IS_OBJECT
-;;;    G_OBJECT_CLASS
-;;;    G_IS_OBJECT_CLASS
-;;;    G_OBJECT_GET_CLASS
-;;;    G_OBJECT_TYPE
-;;;    G_OBJECT_TYPE_NAME
-;;;    G_OBJECT_CLASS_TYPE
-;;;    G_OBJECT_CLASS_NAME
-;;;
-;;;    g_object_class_install_property
-;;;    g_object_class_install_properties
+;;;     g-is-object-class
+
+(test g-is-object-class
+  (is-true  (g-is-object-class (g-type-class-ref "GtkButton"))))
+
+;;;     g-object-class
+
+(test g-object-class
+  (is (g-is-object-class (g-object-class (make-instance 'gtk-button)))))
+
+;;;     g-object-type
+
+(test g-object-type
+  (is (eq (gtype "GtkButton")
+          (g-object-type (make-instance 'gtk-button)))))
+
+;;;     g-object-type-name
+
+(test g-object-type-name
+  (is (string= "GtkButton" (g-object-type-name (make-instance 'gtk-button)))))
+
+;;;     g-object-class-type
+
+(test g-object-class-type
+  (is (eq (gtype "GtkButton")
+          (g-object-class-type (g-object-class (make-instance 'gtk-button))))))
+
+;;;     g-object-class-name
+
+(test g-object-class-name
+  (is (string= "GtkButton"
+               (g-object-class-name (g-object-class (make-instance 'gtk-button))))))
+
+;;;     g_object_class_install_property
+;;;     g_object_class_install_properties
 
 ;;;     g-object-class-find-property
 
@@ -152,39 +109,108 @@
              (mapcar #'g-param-spec-name
                      (g-object-interface-list-properties "GActionGroup")))))
 
-;;;    g_object_new
-;;;    g_object_newv
-;;;
+;;;     g-object-new
 
-;;;
-;;;    g_object_ref
-;;;    g_object_unref
-;;;    g_object_ref_sink
-;;;    g_clear_object
-;;;
-;;;    GInitiallyUnowned
-;;;    GInitiallyUnownedClass
-;;;
-;;;    G_TYPE_INITIALLY_UNOWNED
-;;;
-;;;    g_object_is_floating
-;;;    g_object_force_floating
-;;;    g_object_weak_ref
-;;;    g_object_weak_unref
-;;;    g_object_add_weak_pointer
-;;;    g_object_remove_weak_pointer
-;;;    g_object_add_toggle_ref
-;;;    g_object_remove_toggle_ref
-;;;    g_object_connect
-;;;    g_object_disconnect
-;;;    g_object_set
-;;;    g_object_get
-;;;    g_object_notify
-;;;    g_object_notify_by_pspec
-;;;    g_object_freeze_notify
-;;;    g_object_thaw_notify
+(test g-object-new
+  (is (eq (gtype "GtkButton")
+          (g-object-type (g-object-new "GtkButton"))))
+  (is (eq (gtype "GtkButton")
+          (g-object-type (g-object-new "GtkButton" :label "text" :margin 6)))))
 
-;;;    g-object-data
+;;;     g_object_new_with_properties
+;;;     g_object_newv
+
+;;;     g_object_ref
+;;;     g_object_unref
+;;;     g_object_ref_sink
+;;;     g_clear_object
+
+;;;     GInitiallyUnowned
+;;;     GInitiallyUnownedClass
+
+;;;     G_TYPE_INITIALLY_UNOWNED
+
+;;;     g_object_is_floating
+;;;     g_object_force_floating
+;;;     g_object_weak_ref
+;;;     g_object_weak_unref
+;;;     g_object_add_weak_pointer
+;;;     g_object_remove_weak_pointer
+;;;     g_object_add_toggle_ref
+;;;     g_object_remove_toggle_ref
+
+;;;     g_object_connect
+;;;     g_object_disconnect
+;;;     g_object_set
+;;;     g_object_get
+
+;;;     g-object-notify
+
+(test g-object-notify
+  (let* ((message nil)
+         (button (make-instance 'gtk-button))
+         (handler-id (g-signal-connect button "notify::can-default"
+                       (lambda (widget pspec)
+                         (declare (ignore widget))
+                         (setf message
+                               "Signal notify::can-default in g-object-notify")
+                         (is (g-is-param-spec pspec))
+                       ))))
+    (is (integerp handler-id))
+    ;; Notify button
+    (is-false (g-object-notify button "can-default"))
+    (is (string= "Signal notify::can-default in g-object-notify" message))))
+
+;;;     g_object_notify_by_pspec
+
+;;;     g-object-freeze-notify
+;;;     g-object-thaw-notify
+
+(test g-object-freeze-notify.1
+  (let* ((message nil)
+         (button (make-instance 'gtk-button))
+         (handler-id (g-signal-connect button "notify::can-default"
+                       (lambda (widget pspec)
+                         (declare (ignore widget))
+                         (setf message
+                               (append message (list "notify::can-default")))
+                         (is (g-is-param-spec pspec))))))
+    (is (integerp handler-id))
+    ;; Freeze the notify signal
+    (is-false (g-object-freeze-notify button))
+    (setf message (append message (list "freeze")))
+    ;; Notify button
+    (is-false (g-object-notify button "can-default"))
+    ;; Thaw the notify signal
+    (setf message (append message (list "thaw")))
+    (is-false (g-object-thaw-notify button))
+    ;; Check the order of the execution
+    (is (equal '("freeze" "thaw" "notify::can-default") message))))
+
+;; Counter sample without g-object-freeze-notify
+
+(test g-object-freeze-notify.2
+  (let* ((message nil)
+         (button (make-instance 'gtk-button))
+         (handler-id (g-signal-connect button "notify::can-default"
+                       (lambda (widget pspec)
+                         (declare (ignore widget))
+                         (setf message
+                               (append message (list "notify::can-default")))
+                         (is (g-is-param-spec pspec))))))
+    (is (integerp handler-id))
+    ;; Freeze the notify signal
+;    (g-object-freeze-notify button)
+    (setf message (append message (list "freeze")))
+    ;; Notify button
+    (g-object-notify button "can-default")
+    ;; Thaw the notify signal
+    (setf message (append message (list "thaw")))
+;    (g-object-thaw-notify button)
+    ;; Check the order of the execution
+    (is (equal '("freeze" "notify::can-default" "thaw") message))))
+
+;;;     g-object-data
 
 (test g-object-data
   (let ((button (make-instance 'gtk-button)))
@@ -197,12 +223,12 @@
     (is (pointerp (g-object-data button "property")))
     (is (eq 'gtk-label (type-of (gobject::get-g-object-for-pointer (g-object-data button "property")))))))
 
-;;;    g_object_set_data_full
+;;;     g-object-set-data-full
 
-(defvar *status*)
+(defvar *data-full-status* nil)
 
 (defcallback destroy-notify-cb :void ((data :pointer))
-  (is (string= "destroy-notify-cb" (setf *status* "destroy-notify-cb")))
+  (is (string= "destroy-notify-cb" (setf *data-full-status* "destroy-notify-cb")))
   (is (pointerp data))
   (is (= 100 (pointer-address data))))
 
@@ -214,99 +240,124 @@
                                       (make-pointer 100)
                                       (callback destroy-notify-cb)))
     ;; Clear the status
-    (is-false (setf *status* nil))
+    (is-false (setf *data-full-status* nil))
     ;; Destroy the data, the callback will be executed
     (is (pointerp (setf (g-object-data button "property") (null-pointer))))
     ;; Check status
-    (is (string= "destroy-notify-cb" *status*))))
+    (is (string= "destroy-notify-cb" *data-full-status*))))
 
-;;;    g_object_steal_data
-;;;    g_object_dup_data
-;;;    g_object_replace_data
+;;;     g-object-steal-data
 
-;;;    g_object_get_qdata
-;;;    g_object_set_qdata
-;;;    g_object_set_qdata_full
-;;;    g_object_steal_qdata
-;;;    g_object_dup_qdata
-;;;    g_object_replace-qdata
+(test g-object-steal-data
+  (let ((button (make-instance 'gtk-button)))
+    ;; Set the data
+    (is (pointerp (setf (g-object-data button "property") (make-pointer 100))))
+    (is (= 100 (pointer-address (g-object-data button "property"))))
+    ;; Steal the data
+    (is (= 100 (pointer-address (g-object-steal-data button "property"))))
+    (is (=   0 (pointer-address (g-object-data button "property"))))))
 
-;;;     g-object-set-property
+;;;     g_object_dup_data
+;;;     g_object_replace_data
 
-(test g-object-set-property.1
+;;;     g_object_get_qdata
+;;;     g_object_set_qdata
+;;;     g_object_set_qdata_full
+;;;     g_object_steal_qdata
+;;;     g_object_dup_qdata
+;;;     g_object_replace-qdata
+
+;;;     g-object-property
+
+(test g-object-property.1
   (let ((obj (make-instance 'gtk-label)))
-    (g-object-set-property (pointer obj) "angle" 1.0d0)
-    (is (= 1.0d0 (g-object-get-property (pointer obj) "angle")))
-    (g-object-set-property (pointer obj) "ellipsize" :start)
-    (is (eq :start (g-object-get-property (pointer obj) "ellipsize")))
-    (g-object-set-property (pointer obj) "justify" :fill)
-    (is (eq :fill (g-object-get-property (pointer obj) "justify")))
-    (g-object-set-property (pointer obj) "label" "label")
-    (is (equal "label" (g-object-get-property (pointer obj) "label")))
-    (g-object-set-property (pointer obj) "max-width-chars" 10)
-    (is (= 10 (g-object-get-property (pointer obj) "max-width-chars")))))
+    (is (= 1.0d0 (setf (g-object-property (pointer obj) "angle") 1.0d0)))
+    (is (= 1.0d0 (g-object-property (pointer obj) "angle")))
+    (is (eq :start (setf (g-object-property (pointer obj) "ellipsize") :start)))
+    (is (eq :start (g-object-property (pointer obj) "ellipsize")))
+    (is (eq :fill (setf (g-object-property (pointer obj) "justify") :fill)))
+    (is (eq :fill (g-object-property (pointer obj) "justify")))
+    (is (string= "label" (setf (g-object-property (pointer obj) "label") "label")))
+    (is (string= "label" (g-object-property (pointer obj) "label")))
+    (is (= 10 (setf (g-object-property (pointer obj) "max-width-chars") 10)))
+    (is (= 10 (g-object-property (pointer obj) "max-width-chars")))))
 
-(test g-object-set-property.2
+(test g-object-property.2
   (let ((obj (make-instance 'gtk-label)))
-    (g-object-set-property obj "angle" 1.0d0)
-    (is (= 1.0d0 (g-object-get-property obj "angle")))
-    (g-object-set-property obj "ellipsize" :start)
-    (is (eq :start (g-object-get-property obj "ellipsize")))
-    (g-object-set-property obj "justify" :fill)
-    (is (eq :fill (g-object-get-property obj "justify")))
-    (g-object-set-property obj "label" "label")
-    (is (equal "label" (g-object-get-property obj "label")))
-    (g-object-set-property obj "max-width-chars" 10)
-    (is (= 10 (g-object-get-property obj "max-width-chars")))))
+    (is (= 1.0d0 (setf (g-object-property obj "angle") 1.0d0)))
+    (is (= 1.0d0 (g-object-property obj "angle")))
+    (is (eq :start (setf (g-object-property obj "ellipsize") :start)))
+    (is (eq :start (g-object-property obj "ellipsize")))
+    (is (eq :fill (setf (g-object-property obj "justify") :fill)))
+    (is (eq :fill (g-object-property obj "justify")))
+    (is (string= "label" (setf (g-object-property obj "label") "label")))
+    (is (string= "label" (g-object-property obj "label")))
+    (is (= 10 (setf (g-object-property obj "max-width-chars") 10)))
+    (is (= 10 (g-object-property obj "max-width-chars")))))
 
-;;;     g-object-get-property
-
-(test g-object-get-property.1
+(test g-object-property.3
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (= 0.0d0 (g-object-get-property (pointer obj) "angle")))))
+    (is (= 0.0d0 (g-object-property obj "angle" "gdouble")))
+    (is (= 2.0d0 (setf (g-object-property obj "angle" "gdouble") 2.0d0)))
+    (is (= 2.0d0 (g-object-property obj "angle")))))
 
-;; FIXME: This test causes a critical warning.
-;(test g-object-get-property.2
-;  (let ((obj (make-instance 'gtk-label :label "label")))
-;    (is (eq 'pango-attr-list (type-of (g-object-get-property (pointer obj) "attributes"))))))
-
-(test g-object-get-property.3
+(test g-object-property.4
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (= 0 (g-object-get-property (pointer obj) "cursor-position")))))
+    (is (eq 'pango-attr-list (type-of (g-object-property obj "attributes"))))))
 
-(test g-object-get-property.4
+(test g-object-property.5
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (eq :none (g-object-get-property (pointer obj) "ellipsize")))))
+    (is (= 0 (g-object-property obj "cursor-position" "gint")))
+    ;; cursor-position is not writable
+;    (is (= 2 (setf (g-object-property obj "cursor-position" "gint") 2)))
+;    (is (= 2 (g-object-property obj "cursor-position")))
+  ))
 
-(test g-object-get-property.5
+(test g-object-property.6
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (eq :left (g-object-get-property (pointer obj) "justify")))))
+    (is (eq :none (g-object-property obj "ellipsize" "PangoEllipsizeMode")))
+    (is (eq :start (setf (g-object-property obj "ellipsize" "PangoEllipsizeMode") :start)))
+    (is (eq :start (g-object-property obj "ellipsize")))))
 
-(test g-object-get-property.6
+(test g-object-property.7
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (equal "label" (g-object-get-property (pointer obj) "label")))))
+    (is (eq :left (g-object-property obj "justify")))
+    (is (eq :center (setf (g-object-property obj "justify" "GtkJustification") :center)))
+    (is (eq :center (g-object-property obj "justify")))))
 
-(test g-object-get-property.7
+(test g-object-property.8
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (= -1 (g-object-get-property (pointer obj) "max-width-chars")))))
+    (is (string= "label" (g-object-property obj "label")))
+    (is (string= "text" (setf (g-object-property obj "label" "gchararray") "text")))
+    (is (string= "text" (g-object-property obj "label")))))
 
-(test g-object-get-property.8
+(test g-object-property.9
   (let ((obj (make-instance 'gtk-label :label "label")))
-    (is (= 16777215 (g-object-get-property (pointer obj) "mnemonic-keyval")))))
+    (is (= -1 (g-object-property obj "max-width-chars" "gint")))
+    (is (= 10 (setf (g-object-property obj "max-width-chars" "gint") 10)))
+    (is (= 10 (g-object-property obj "max-width-chars")))))
 
-;;;    g_object_new_valist
-;;;    g_object_set_valist
-;;;    g_object_get_valist
-;;;    g_object_watch_closure
-;;;    g_object_run_dispose
-;;;
-;;;    G_OBJECT_WARN_INVALID_PROPERTY_ID
-;;;
-;;;    GWeakRef
-;;;
-;;;    g_weak_ref_init
-;;;    g_weak_ref_clear
-;;;    g_weak_ref_get
-;;;    g_weak_ref_set
+(test g-object-property.10
+  (let ((obj (make-instance 'gtk-label :label "label")))
+    (is (= 16777215 (g-object-property obj "mnemonic-keyval" "guint")))
+    ;; mnemonic-keyval is not writable
+;    (is (= 10000000 (setf (g-object-property obj "mnemonic-keyval" "guint") 10000000)))
+;    (is (= 10000000 (g-object-property obj "mnmonic-keyval")))
+  ))
 
-;;; 2020-10-15
+;;;     g_object_new_valist
+;;;     g_object_set_valist
+;;;     g_object_get_valist
+;;;     g_object_watch_closure
+;;;     g_object_run_dispose
+
+;;;     G_OBJECT_WARN_INVALID_PROPERTY_ID
+
+;;;     GWeakRef
+;;;     g_weak_ref_init
+;;;     g_weak_ref_clear
+;;;     g_weak_ref_get
+;;;     g_weak_ref_set
+;;;     g_assert_finalize_object
+
+;;; 2020-10-17
