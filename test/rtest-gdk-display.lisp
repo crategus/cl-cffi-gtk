635a65be9b76d0279269ef8916f6f3e7072e6f82
crategus <crategus@users.sourceforge.net>
2020-11-07 23:59:37 +0100

    Work on the testsuite for GdkDisplay

--- test/rtest-gdk-display.lisp
+++ test/rtest-gdk-display.lisp
@@ -1,198 +1,336 @@
 (def-suite gdk-display :in gdk-suite)
 (in-suite gdk-display)
 
+;;; --- Types and Values -------------------------------------------------------
+
 ;;;     GdkDisplay
 
 (test gdk-display-class
-  ;; Type checks
-  (is-true  (g-type-is-object "GdkDisplay"))
-  (is-false (g-type-is-abstract "GdkDisplay"))
-  (is-true  (g-type-is-derived "GdkDisplay"))
-  (is-false (g-type-is-fundamental "GdkDisplay"))
-  (is-true  (g-type-is-value-type "GdkDisplay"))
-  (is-true  (g-type-has-value-table "GdkDisplay"))
-  (is-true  (g-type-is-classed "GdkDisplay"))
-  (is-true  (g-type-is-instantiatable "GdkDisplay"))
-  (is-true  (g-type-is-derivable "GdkDisplay"))
-  (is-true  (g-type-is-deep-derivable "GdkDisplay"))
-  (is-false (g-type-is-interface "GdkDisplay"))
-
+  ;; Type check
+  (is (g-type-is-object "GdkDisplay"))
   ;; Check the registered name
   (is (eq 'gdk-display
           (registered-object-type-by-name "GdkDisplay")))
-
-  ;; Check infos about the C class implementation
-  (let ((class (g-type-class-ref (gtype "GdkDisplay"))))
-    (is (equal (gtype "GdkDisplay") (g-type-from-class class)))
-    (is (equal (gtype "GdkDisplay") (g-object-class-type class)))
-    (is (equal "GdkDisplay" (g-object-class-name class)))
-    (is (equal (gtype "GdkDisplay")
-               (g-type-from-class  (g-type-class-peek "GdkDisplay"))))
-    (is (equal (gtype "GdkDisplay")
-               (g-type-from-class (g-type-class-peek-static "GdkDisplay"))))
-    (g-type-class-unref class))
-
-  ;; Check infos about the Lisp class implementation
-  (let ((class (find-class 'gdk-display)))
-    ;; Check the class name and type of the class
-    (is (eq 'gdk-display (class-name class)))
-    (is (eq 'gobject-class (type-of class)))
-    (is (eq (find-class 'gobject-class) (class-of class)))
-    ;; Properties of the metaclass gobject-class
-    (is (equal "GdkDisplay" (gobject-class-g-type-name class)))
-    (is (equal "GdkDisplay" (gobject-class-direct-g-type-name class)))
-    (is (equal "gdk_display_get_type"
-               (gobject-class-g-type-initializer class)))
-    (is-false (gobject-class-interface-p class)))
-
-  ;; Check some more GType information
-  (is (equal (gtype "GObject") (g-type-parent "GdkDisplay")))
-  (is (= 2 (g-type-depth "GdkDisplay")))
-  (is (equal (gtype "GdkDisplay")
-             (g-type-next-base "GdkDisplay" "GObject")))
-  (is-true  (g-type-is-a "GdkDisplay" "GObject"))
-  (is-false (g-type-is-a "GdkDisplay" "GtkWidget"))
-  (is-false (g-type-is-a "GdkDisplay" "gboolean"))
-
+  ;; Check the type initializer
+  (is (eq (gtype "GdkDisplay")
+          (gtype (foreign-funcall "gdk_display_get_type" g-size))))
+  ;; Check the parent
+  (is (eq (gtype "GObject") (g-type-parent "GdkDisplay")))
   ;; Check the children
   (is (equal '("GdkX11Display" "GdkBroadwayDisplay" "GdkWaylandDisplay")
              (mapcar #'gtype-name (g-type-children "GdkDisplay"))))
-
   ;; Check the interfaces
   (is (equal '()
              (mapcar #'gtype-name (g-type-interfaces "GdkDisplay"))))
-
-  ;; Query infos about the class
-  (with-foreign-object (query '(:struct g-type-query))
-    (g-type-query "GdkDisplay" query)
-    (is (equal (gtype "GdkDisplay")
-               (foreign-slot-value query '(:struct g-type-query) :type)))
-    (is (equal "GdkDisplay"
-               (foreign-slot-value query '(:struct g-type-query) :type-name)))
-    (is (= 536 (foreign-slot-value query '(:struct g-type-query) :class-size)))
-    (is (= 136 (foreign-slot-value query '(:struct g-type-query) :instance-size))))
-
-  ;; Get the names of the class properties.
+  ;; Check the class properties
   (is (equal '()
-             (mapcar #'g-param-spec-name
-                     (g-object-class-list-properties "GdkDisplay"))))
-
-  ;; No style properties
-  ;; No child properties
-
-  ;; Get the class definition
+             (stable-sort (mapcar #'g-param-spec-name
+                                  (g-object-class-list-properties "GdkDisplay"))
+                          #'string-lessp)))
+  ;; Check the class definition
   (is (equal '(DEFINE-G-OBJECT-CLASS "GdkDisplay" GDK-DISPLAY
                        (:SUPERCLASS G-OBJECT :EXPORT T :INTERFACES NIL
                         :TYPE-INITIALIZER "gdk_display_get_type")
                        NIL)
              (get-g-type-definition "GdkDisplay"))))
 
-;;; ----------------------------------------------------------------------------
-;;; Functions
-;;; ----------------------------------------------------------------------------
+;;; --- Signals ----------------------------------------------------------------
 
-;;;     gdk_display_open
+;;;     closed
+
+(test gdk-display-closed-signal
+  (let* ((message nil)
+         (display (gdk-display-default))
+         (handler-id (g-signal-connect display "closed"
+                       (lambda (display is-error)
+                         (setf message "Signal closed")
+                         (is (typep display 'gdk-display))
+                         (is-true is-error)
+                         t))))
+    ;; Emit the signal
+    (is-false (g-signal-emit display "closed" t))
+    (is (string= "Signal closed" message))
+    (is-false (g-signal-handler-disconnect display handler-id))))
+
+;;;     monitor-added
+
+(test gdk-display-monitor-added-signal
+  (let* ((message nil)
+         (display (gdk-display-default))
+         (handler-id (g-signal-connect display "monitor-added"
+                       (lambda (display monitor)
+                         (setf message "Signal monitor-added")
+                         (is (typep display 'gdk-display))
+                         (is (typep monitor 'gdk-monitor))
+                         t))))
+    ;; Emit the signal
+    (is-false (g-signal-emit display "monitor-added"
+                                     (gdk-display-monitor display 0)))
+    (is (string= "Signal monitor-added" message))
+    (is-false (g-signal-handler-disconnect display handler-id))))
+
+;;;     monitor-removed
+
+(test gdk-display-monitor-removed-signal
+  (let* ((message nil)
+         (display (gdk-display-default))
+         (handler-id (g-signal-connect display "monitor-removed"
+                       (lambda (display monitor)
+                         (setf message "Signal monitor-removed")
+                         (is (typep display 'gdk-display))
+                         (is (typep monitor 'gdk-monitor))
+                         t))))
+    ;; Emit the signal
+    (is-false (g-signal-emit display "monitor-removed"
+                                     (gdk-display-monitor display 0)))
+    (is (string= "Signal monitor-removed" message))
+    (is-false (g-signal-handler-disconnect display handler-id))))
+
+;;;     opened
+
+(test gdk-display-opened-signal
+  (let* ((message nil)
+         (display (gdk-display-default))
+         (handler-id (g-signal-connect display "opened"
+                       (lambda (display)
+                         (setf message "Signal opened")
+                         (is (typep display 'gdk-display))
+                         t))))
+    ;; Emit the signal
+    (is-false (g-signal-emit display "opened"))
+    (is (string= "Signal opened" message))
+    (is-false (g-signal-handler-disconnect display handler-id))))
+
+;;;     seat-added
+
+(test gdk-display-seat-added-signal
+  (let* ((message nil)
+         (display (gdk-display-default))
+         (handler-id (g-signal-connect display "seat-added"
+                       (lambda (display seat)
+                         (setf message "Signal seat-added")
+                         (is (typep display 'gdk-display))
+                         (is (typep seat 'gdk-seat))
+                         t))))
+    ;; Emit the signal
+    (is-false (g-signal-emit display "seat-added"
+                                     (gdk-display-default-seat display)))
+    (is (string= "Signal seat-added" message))
+    (is-false (g-signal-handler-disconnect display handler-id))))
+
+;;;     seat-removed
+
+(test gdk-display-seat-removed-signal
+  (let* ((message nil)
+         (display (gdk-display-default))
+         (handler-id (g-signal-connect display "seat-removed"
+                       (lambda (display seat)
+                         (setf message "Signal seat-removed")
+                         (is (typep display 'gdk-display))
+                         (is (typep seat 'gdk-seat))
+                         t))))
+    ;; Emit the signal
+    (is-false (g-signal-emit display "seat-removed"
+                                     (gdk-display-default-seat display)))
+    (is (string= "Signal seat-removed" message))
+    (is-false (g-signal-handler-disconnect display handler-id))))
+
+;;; --- Functions --------------------------------------------------------------
+
+;;;     gdk-display-open
 
 (test gdk-display-open
-  (let ((name (gdk-display-get-name (gdk-display-default))))
-    (is-true (gdk-display-open name))
-    (is-false (gdk-display-open "test"))
-))
+  (let ((name (gdk-display-name (gdk-display-default))))
+    (is-true (gdk-display-open name))))
 
-;;;     gdk_display_get_default
+;;;     gdk-display-default
 
 (test gdk-display-default
-  (is (eq 'gdk-display (type-of (gdk-display-default)))))
+  (is (typep (gdk-display-default) 'gdk-display)))
 
-;;;     gdk_display_get_name
+;;;     gdk-display-name
 
-(test gdk-display-get-name
-  (is (equal ":0" (gdk-display-get-name (gdk-display-default)))))
+(test gdk-display-name
+  (is (string= ":0" (gdk-display-name (gdk-display-default)))))
 
-;;;     gdk_display_get_n_screens                     * deprecated
-;;;     gdk_display_get_screen                        * deprecated
+;;;     gdk_display_get_n_screens                          deprecated
+;;;     gdk_display_get_screen                             deprecated
 
-;;;     gdk_display_get_default_screen
+;;;     gdk-display-default-screen
 
 (test gdk-display-default-screen
   (let ((display (gdk-display-default)))
-    (is (eq 'gdk-screen (type-of (gdk-display-default-screen display))))))
+    (is (typep (gdk-display-default-screen display) 'gdk-screen))))
 
-;;;     gdk_display_get_device_manager                * deprecated
-;;;     gdk_display_pointer_ungrab                    * deprecated
-;;;     gdk_display_keyboard_ungrab                   * deprecated
-;;;     gdk_display_pointer_is_grabbed                * deprecated
+;;;     gdk_display_get_device_manager                     deprecated
+;;;     gdk_display_pointer_ungrab                         deprecated
+;;;     gdk_display_keyboard_ungrab                        deprecated
+;;;     gdk_display_pointer_is_grabbed                     deprecated
 
 ;;;     gdk_display_device_is_grabbed
 
+;; TODO: The grap is successfully but it is not released.
 
+#+nil
+(test gdk-display-device-is-grabbed
+  (let* ((display (gdk-display-default))
+         (seat (gdk-display-default-seat display))
+         (device (gdk-seat-pointer seat))
+         (window (gdk-default-root-window)))
+    (is-false (gdk-display-device-is-grabbed display device))
+    (is-false (gdk-window-show window))
+    (is (eq :success (gdk-seat-grab seat window :pointer nil nil nil nil)))
+    (is-true (gdk-display-device-is-grabbed display device))
+    (is-false (gdk-seat-ungrab seat))
+    (is-false (gdk-display-device-is-grabbed display device))))
 
-;;;     gdk_display_beep
-;;;     gdk_display_sync
-;;;     gdk_display_flush
+;;;     gdk-display-beep
 
-;;;     gdk_display_close
+(test gdk-display-beep
+  (is-false (gdk-display-beep (gdk-display-default))))
 
+;;;     gdk-display-sync
 
+(test gdk-display-sync
+  (is-false (gdk-display-sync (gdk-display-default))))
 
-;;;     gdk_display_is_closed
+;;;     gdk-display-flush
+
+(test gdk-display-flush
+  (is-false (gdk-display-flush (gdk-display-default))))
+
+;;;     gdk-display-close
+;;;     gdk-display-is-closed
+
+(test gdk-display-close
+  (let ((display (gdk-display-default)))
+    (is-false (gdk-display-is-closed display))
+    (is-false (gdk-display-close display))
+    (is-true (gdk-display-is-closed display))))
 
 ;;;     gdk_display_get_event
 ;;;     gdk_display_peek_event
 ;;;     gdk_display_put_event
 ;;;     gdk_display_has_pending
-;;;     gdk_display_set_double_click_time
-;;;     gdk_display_set_double_click_distance
-;;;     gdk_display_get_pointer                       * deprecated
-;;;     gdk_display_list_devices                      * deprecated
-;;;     gdk_display_get_window_at_pointer             * deprecated
-;;;     gdk_display_warp_pointer                      * deprecated
-;;;     gdk_display_supports_cursor_color
-;;;     gdk_display_supports_cursor_alpha
-;;;     gdk_display_get_default_cursor_size
-;;;     gdk_display_get_maximal_cursor_size
-;;;     gdk_display_get_default_group
-;;;     gdk_display_supports_selection_notification
-;;;     gdk_display_request_selection_notification
-;;;     gdk_display_supports_clipboard_persistence
-;;;     gdk_display_store_clipboard
-;;;     gdk_display_supports_shapes
-;;;     gdk_display_supports_input_shapes
-;;;     gdk_display_supports_composite                * deprecated
-;;;     gdk_display_get_app_launch_context
-;;;     gdk_display_notify_startup_complete
 
-;;;     gdk_display_get_default_seat
+;;;     gdk-display-set-double-click-time
+;;;     gdk-display-set-double-click-distance
+
+(test gdk-display-double-click
+  (let ((display (gdk-display-default)))
+    (is-false (gdk-display-set-double-click-time display 500))
+    (is-false (gdk-display-set-double-click-distance display 10))))
+
+;;;     gdk_display_get_pointer                            deprecated
+;;;     gdk_display_list_devices                           deprecated
+;;;     gdk_display_get_window_at_pointer                  deprecated
+;;;     gdk_display_warp_pointer                           deprecated
+
+;;;     gdk-display-supports-cursor-color
+;;;     gdk-display-supports-cursor-alpha
+
+(test gdk-display-supports-cursor
+  (let ((display (gdk-display-default)))
+    (is-true (gdk-display-supports-cursor-color display))
+    (is-true (gdk-display-supports-cursor-alpha display))))
+
+;;;     gdk-display-default-cursor-size
+;;;     gdk-display-maximal-cursor-size
+
+(test gdk-display-cursor-size
+  (let ((display (gdk-display-default)))
+    (is (= 24 (gdk-display-default-cursor-size display)))
+    (is (equal '(128 128)
+                (multiple-value-list (gdk-display-maximal-cursor-size display))))))
+
+;;;     gdk-display-default-group
+
+(test gdk-display-default-group
+  (let ((display (gdk-display-default)))
+    (is (typep (gdk-display-default-group display) 'gdk-window))))
+
+;;;     gdk-display-supports-selection-notification
+;;;     gdk-display-request-selection-notification
+
+(test gdk-display-selection-notification
+  (let ((display (gdk-display-default)))
+    (is-true (gdk-display-supports-selection-notification display))
+    (is-true (gdk-display-request-selection-notification display "CLIPBOARD"))))
+
+;;;     gdk-display-supports-clipboard-persistence
+;;;     gdk-display-store-clipboard
+
+(test gdk-display-clipboard
+  (let* ((display (gdk-display-default))
+         (window (gdk-display-default-group display)))
+    (is-false (gdk-display-supports-clipboard-persistence display))
+    (is-false (gdk-display-store-clipboard display window 0 '("ClIPBOARD")))))
+
+;;;     gdk-display-supports-shapes
+;;;     gdk-display-supports-input_shapes
+
+(test gdk-display-shapes
+  (let ((display (gdk-display-default)))
+    (is-true (gdk-display-supports-shapes display))
+    (is-true (gdk-display-supports-input-shapes display))))
+
+;;;     gdk_display_supports_composite                     deprecated
+
+;;;     gdk-display-app-launch-context
+
+(test gdk-display-app-launch-context
+  (let ((display (gdk-display-default)))
+    (is (typep (gdk-display-app-launch-context display) 'gdk-app-launch-context))))
+
+;;;     gdk-display-notify-startup-complete
+
+(test gdk-display-notify-startup-complete
+  (let ((display (gdk-display-default)))
+    (is-false (gdk-display-notify-startup-complete display "crategus.com"))))
+
+;;;     gdk-display-default-seat
 
 (test gdk-display-default-seat
   (let ((display (gdk-display-default)))
-    (is (eq 'gdk-seat (type-of (gdk-display-default-seat display))))))
+    (is (typep (gdk-display-default-seat display) 'gdk-seat))))
 
-;;;     gdk_display_list_seats
+;;;     gdk-display-list-seats
 
 (test gdk-display-list-seats
   (let ((display (gdk-display-default)))
     (is (listp (gdk-display-list-seats display)))
-    (is (eq 'gdk-seat (type-of (first (gdk-display-list-seats display)))))))
+    (is (every (lambda (x) (typep x 'gdk-seat))
+               (gdk-display-list-seats display)))))
 
-;;;     gdk_display_get_n_monitors
+;;;     gdk-display-n-monitors
 
-(test gdk-display-get-n-monitors
+(test gdk-display-n-monitors
   (let ((display (gdk-display-default)))
-    (is (equal 1 (gdk-display-get-n-monitors display)))))
+    (is (<= 1 (gdk-display-n-monitors display)))))
 
-;;;     gdk_display_get_monitor
+;;;     gdk-display-monitor
 
-(test gdk-display-get-monitor
+(test gdk-display-monitor
   (let ((display (gdk-display-default)))
-    (is (eq 'gdk-monitor (type-of (gdk-display-get-monitor display 0))))))
+    (is (typep (gdk-display-monitor display 0) 'gdk-monitor))))
 
-;;;     gdk_display_get_primary_monitor
+;;;     gdk-display-primary-monitor
 
-(test gdk-display-get-primary-monitor
+(test gdk-display-primary-monitor
   (let ((display (gdk-display-default)))
-    (is (eq 'gdk-monitor (type-of (gdk-display-get-primary-monitor display))))))
+    (is (typep (gdk-display-primary-monitor display) 'gdk-monitor))))
 
-;;;     gdk_display_get_monitor_at_point
-;;;     gdk_display_get_monitor_at_window
+;;;     gdk-display-monitor-at-point
 
+(test gdk-display-monitor-at-point
+  (let ((display (gdk-display-default)))
+    (is (typep (gdk-display-monitor-at-point display 0 0) 'gdk-monitor))))
+
+;;;     gdk-display-monitor-at-window
+
+(test gdk-display-monitor-at-window
+  (let* ((display (gdk-display-default))
+         (window (gdk-display-default-group display)))
+    (is (typep (gdk-display-monitor-at-window display window) 'gdk-monitor))))
+
+;;; 2020-11-7
