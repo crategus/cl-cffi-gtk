9784de7e9c2049266ea1817d552757d8e13c11c3
crategus <crategus@users.sourceforge.net>
2020-11-09 23:14:55 +0100

    Work on the implementation and documentation of GdkDevicePad

--- gdk/gdk.device-pad.lisp
+++ gdk/gdk.device-pad.lisp
@@ -6,7 +6,7 @@
 ;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
 ;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
-;;; Copyright (C) 2019 Dieter Kaiser
+;;; Copyright (C) 2019 - 2020 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -68,7 +68,7 @@
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gdk-device-pad-feature atdoc:*symbol-name-alias*) "Enum"
       (gethash 'gdk-device-pad-feature atdoc:*external-symbols*)
- "@version{2019-3-25}
+ "@version{2020-11-9}
   @begin{short}
     A pad feature.
   @end{short}
@@ -86,7 +86,7 @@
     @entry[:strip]{A straight interactive area.}
   @end{table}
   Since 3.22
-  @see-class{gdk-monitor}")
+  @see-class{gdk-device-pad}")
 
 ;;; ----------------------------------------------------------------------------
 ;;; struct GdkDevicePad
@@ -99,7 +99,7 @@
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gdk-device-pad atdoc:*class-name-alias*) "Interface"
       (documentation 'gdk-device-pad 'type)
- "@version{2019-3-25}
+ "@version{2020-11-9}
   @begin{short}
     @sym{gdk-device-pad} is an interface implemented by devices of type
     @code{:tablet-pad}, it allows querying the features provided by the pad
@@ -107,104 +107,110 @@
   @end{short}
 
   Tablet pads may contain one or more groups, each containing a subset of the
-  buttons/rings/strips available. The function @fun{gdk-device-pad-get-n-groups}
+  buttons/rings/strips available. The function @fun{gdk-device-pad-n-groups}
   can be used to obtain the number of groups, the functions
-  @fun{gdk-device-pad-get-n-features} and @fun{gdk-device-pad-get-feature-group}
-  can be combined to find out the number of buttons/rings/strips the device has,
+  @fun{gdk-device-pad-n-features} and @fun{gdk-device-pad-feature-group} can
+  be combined to find out the number of buttons/rings/strips the device has,
   and how are they grouped.
 
   Each of those groups have different modes, which may be used to map each
   individual pad feature to multiple actions. Only one mode is effective
   (current) for each given group, different groups may have different current
   modes. The number of available modes in a group can be found out through the
-  function @fun{gdk-device-pad-get-group-n-modes}, and the current mode for a
+  function @fun{gdk-device-pad-group-n-modes}, and the current mode for a
   given group will be notified through the @class{gdk-event-pad-group-mode}
-  event.")
+  event.
+  @see-class{gdk-device}")
 
 ;;; ----------------------------------------------------------------------------
-;;; gdk_device_pad_get_n_groups ()
-;;;
-;;; gint gdk_device_pad_get_n_groups (GdkDevicePad *pad);
-;;;
-;;; Returns the number of groups this pad device has. Pads have at least one
-;;; group. A pad group is a subcollection of buttons/strip/rings that is
-;;; affected collectively by a same current mode.
-;;;
-;;; pad :
-;;;     a GdkDevicePad
-;;;
-;;; Returns :
-;;;     The number of button/ring/strip groups in the pad.
-;;;
-;;; Since: 3.22
+;;; gdk_device_pad_get_n_groups () -> gdk-device-pad-n-groups
 ;;; ----------------------------------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; gdk_device_pad_get_group_n_modes ()
-;;;
-;;; gint
-;;; gdk_device_pad_get_group_n_modes (GdkDevicePad *pad,
-;;;                                   gint group_idx);
-;;;
-;;; Returns the number of modes that group may have.
-;;;
-;;; pad :
-;;;     a GdkDevicePad
-;;;
-;;; group_idx :
-;;;     group to get the number of available modes from
-;;;
-;;; Returns :
-;;;     The number of modes available in group .
-;;;
-;;; Since: 3.22
-;;; ----------------------------------------------------------------------------
+(defcfun ("gdk_device_pad_get_n_groups" gdk-device-pad-n-groups) :int
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-9}
+  @argument[pad]{a @class{gdk-device-pad} object}
+  @return{An integer with the number of button/ring/strip groups in the pad.}
+  @begin{short}
+    Returns the number of groups this pad device has.
+  @end{short}
+  Pads have at least one group. A pad group is a subcollection of
+  buttons/strip/rings that is affected collectively by a same current mode.
+
+  Since 3.22
+  @see-class{gdk-device-pad}"
+  (pad (g-object gdk-device-pad)))
+
+(export 'gdk-device-pad-n-groups)
 
 ;;; ----------------------------------------------------------------------------
-;;; gdk_device_pad_get_n_features ()
-;;;
-;;; gint
-;;; gdk_device_pad_get_n_features (GdkDevicePad *pad,
-;;;                                GdkDevicePadFeature feature);
-;;;
-;;; Returns the number of features a tablet pad has.
-;;;
-;;; pad :
-;;;     a GdkDevicePad
-;;;
-;;; feature :
-;;; a pad feature
-;;;
-;;; Returns
-;;;     The amount of elements of type feature that this pad has.
-;;;
-;;; Since: 3.22
+;;; gdk_device_pad_get_group_n_modes () -> gdk-device-pad-group-n-modes
 ;;; ----------------------------------------------------------------------------
 
+(defcfun ("gdk_device_pad_get_group_n_modes" gdk-device-pad-group-n-modes) :int
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-9}
+  @argument[pad]{a @class{gdk-device-pad} object}
+  @argument[index]{an integer with the group to get the number of available
+    modes from}
+  @return{An ingeter with the number of modes available in the group,}
+  @begin{short}
+    Returns the number of modes that the group may have.
+  @end{short}
+
+  Since 3.22
+  @see-class{gdk-device-pad}"
+  (pad (g-object gdk-device-pad))
+  (index :int))
+
+(export 'gdk-device-pad-group-n-modes)
+
 ;;; ----------------------------------------------------------------------------
-;;; gdk_device_pad_get_feature_group ()
-;;;
-;;; gint
-;;; gdk_device_pad_get_feature_group (GdkDevicePad *pad,
-;;;                                   GdkDevicePadFeature feature,
-;;;                                   gint feature_idx);
-;;;
-;;; Returns the group the given feature and idx belong to, or -1 if
-;;; feature/index do not exist in pad .
-;;;
-;;; pad :
-;;;     a GdkDevicePad
-;;;
-;;; feature :
-;;;     the feature type to get the group from
-;;;
-;;; feature_idx
-;;;     the index of the feature to get the group from
-;;;
-;;; Returns :
-;;;     The group number of the queried pad feature.
-;;;
-;;; Since: 3.22
+;;; gdk_device_pad_get_n_features () -> gdk-device-pad-n-features
 ;;; ----------------------------------------------------------------------------
+
+(defcfun ("gdk_device_pad_get_n_features" gdk-device-pad-n-features) :int
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-9}
+  @argument[pad]{a @class{gdk-device-pad} object}
+  @argument[feature]{a pad feature of type @symbol{gdk-device-pad-feature}}
+  @return{An integer with the amount of elements of type feature that this pad
+    has.}
+  @begin{short}
+    Returns the number of features a tablet pad has.
+  @end{short}
+
+  Since 3.22
+  @see-class{gdk-device-pad}"
+  (pad (g-object gdk-device-pad))
+  (feature gdk-device-pad-feature))
+
+(export 'gdk-device-pad-n-features)
+
+;;; ----------------------------------------------------------------------------
+;;; gdk_device_pad_get_feature_group () -> gdk-device-pad-feature-group
+;;; ----------------------------------------------------------------------------
+
+(defcfun ("gdk_device_pad_get_feature_group" gdk-device-pad-feature-group) :int
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-9}
+  @argument[pad]{a @class{gdk-device-pad} object}
+  @argument[feature]{the pad feature of type @symbol{gdk-device-pad-feature}
+    to get the group from}
+  @argument[index]{an integer with the index of the feature to get the
+    group from}
+  @return{An integer with the group number of the queried pad feature.}
+  @begin{short}
+    Returns the group the given feature and @arg{index} belong to, or -1 if
+    feature/index do not exist in pad .
+  @end{short}
+
+  Since 3.22
+  @see-class{gdk-device-pad}"
+  (pad (g-object gdk-device-pad))
+  (feature gdk-device-pad-feature)
+  (index :int))
+
+(export 'gdk-device-pad-feature-group)
 
 ;;; --- End of file gdk.device-pad.lisp ----------------------------------------
