5ccae81a9f792d103d009a2d90594fb2be955a33
crategus <crategus@users.sourceforge.net>
2020-11-11 19:54:04 +0100

    Work on the implementation and documentation

--- gdk/gdk.pixbuf.lisp
+++ gdk/gdk.pixbuf.lisp
@@ -6,7 +6,7 @@
 ;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
 ;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
-;;; Copyright (C) 2012 - 2016 Dieter Kaiser
+;;; Copyright (C) 2012 - 2020 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -47,26 +47,26 @@
 (in-package :gdk)
 
 ;;; ----------------------------------------------------------------------------
-;;; gdk_pixbuf_get_from_window ()
+;;; gdk_pixbuf_get_from_window () -> gdk-pixbuf-from-window
 ;;; ----------------------------------------------------------------------------
 
-(defcfun ("gdk_pixbuf_get_from_window" gdk-pixbuf-get-from-window)
+(defcfun ("gdk_pixbuf_get_from_window" gdk-pixbuf-from-window)
     (g-object gdk-pixbuf)
  #+cl-cffi-gtk-documentation
- "@version{2013-7-3}
-  @argument[window]{source window}
-  @argument[src-x]{source x coordinate within window}
-  @argument[src-y]{source y coordinate within window}
-  @argument[width]{width in pixels of region to get}
-  @argument[height]{height in pixels of region to get}
-  @return{A newly-created pixbuf with a reference count of 1, or @code{nil} on
-    error.}
+ "@version{2020-11-10}
+  @argument[window]{a @class{gdk-window} source window}
+  @argument[src-x]{an integer with the source x coordinate within window}
+  @argument[src-y]{an integer with the source y coordinate within window}
+  @argument[width]{an integer with the width in pixels of region to get}
+  @argument[height]{an integer with the height in pixels of region to get}
+  @return{A newly-created @class{gdk-pixbuf} object, or @code{nil} on error.}
   @begin{short}
-    Transfers image data from a @class{gdk-window} and converts it to an
-    RGB(A) representation inside a @class{gdk-pixbuf}. In other words, copies
-    image data from a server-side drawable to a client-side RGB(A) buffer. This
-    allows you to efficiently read individual pixels on the client side.
+    Transfers image data from a @class{gdk-window} object and converts it to an
+    RGB(A) representation inside a @class{gdk-pixbuf} object.
   @end{short}
+  In other words, copies image data from a server-side drawable to a client-side
+  RGB(A) buffer. This allows you to efficiently read individual pixels on the
+  client side.
 
   This function will create an RGB pixbuf with 8 bits per channel with the
   same size specified by the width and height arguments. The pixbuf will
@@ -88,47 +88,50 @@
 
   In short, there are several ways this function can fail, and if it fails
   it returns @code{nil}; so check the return value.
-  @see-function{gdk-pixbuf-get-from-surface}"
+  @see-class{gdk-window}
+  @see-class{gdk-pixbuf}
+  @see-function{gdk-pixbuf-from-surface}"
   (window (g-object gdk-window))
   (src-x :int)
   (src-y :int)
   (width :int)
   (height :int))
 
-(export 'gdk-pixbuf-get-from-window)
+(export 'gdk-pixbuf-from-window)
 
 ;;; ----------------------------------------------------------------------------
-;;; gdk_pixbuf_get_from_surface ()
+;;; gdk_pixbuf_get_from_surface () -> gdk-pixbuf-from-surface
 ;;; ----------------------------------------------------------------------------
 
-(defcfun ("gdk_pixbuf_get_from_surface" gdk-pixbuf-get-from-surface)
+(defcfun ("gdk_pixbuf_get_from_surface" gdk-pixbuf-from-surface)
     (g-object gdk-pixbuf)
  #+cl-cffi-gtk-documentation
- "@version{2013-7-3}
-  @argument[surface]{surface to copy from}
-  @argument[src-x]{source x coordinate within surface}
-  @argument[src-y]{source y coordinate within surface}
-  @argument[width]{width in pixels of region to get}
-  @argument[height]{height in pixels of region to get}
-  @return{A newly-created pixbuf with a reference count of 1,
-    or @code{nil} on error.}
+ "@version{2020-11-10}
+  @argument[surface]{a @symbol{cairo-surface-t} structure to copy from}
+  @argument[src-x]{an integer with the source x coordinate within surface}
+  @argument[src-y]{an integer with the source y coordinate within surface}
+  @argument[width]{an integer with width in pixels of region to get}
+  @argument[height]{an integer with the height in pixels of region to get}
+  @return{A newly-created @class{gdk-pixbuf} object, or @code{nil} on error.}
   @begin{short}
     Transfers image data from a @symbol{cairo-surface-t} structure and converts
     it to an RGB(A) representation inside a @class{gdk-pixbuf} object.
   @end{short}
-  This allows you to efficiently read individual pixels from cairo surfaces. For
-  @class{gdk-window}'s, use the function @fun{gdk-pixbuf-get-from-window}
-  instead.
+  This allows you to efficiently read individual pixels from cairo surfaces.
+  For @class{gdk-window} objects, use the function
+  @fun{gdk-pixbuf-from-window} instead.
 
   This function will create an RGB pixbuf with 8 bits per channel. The pixbuf
   will contain an alpha channel if the surface contains one.
-  @see-function{gdk-pixbuf-get-from-window}"
+  @see-symbol{cairo-surface-t}
+  @see-class{gdk-pixbuf}
+  @see-function{gdk-pixbuf-from-window}"
   (surface (:pointer (:struct cairo-surface-t)))
   (src-x :int)
   (src-y :int)
   (width :int)
   (height :int))
 
-(export 'gdk-pixbuf-get-from-surface)
+(export 'gdk-pixbuf-from-surface)
 
 ;;; --- End of file gdk.pixbuf.lisp --------------------------------------------
