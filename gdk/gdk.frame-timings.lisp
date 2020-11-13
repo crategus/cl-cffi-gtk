921241f1ad78e1b9424b86e1b1ecadef5431cf85
crategus <crategus@users.sourceforge.net>
2020-11-13 11:46:24 +0100

    Work on the implementation and documentation of GdkFrameTimings

--- gdk/gdk.frame-timings.lisp
+++ gdk/gdk.frame-timings.lisp
@@ -6,7 +6,7 @@
 ;;; See <http://www.gtk.org>. The API documentation of the Lisp binding is
 ;;; available from <http://www.crategus.com/books/cl-cffi-gtk/>.
 ;;;
-;;; Copyright (C) 2016 - 2019 Dieter Kaiser
+;;; Copyright (C) 2016 - 2020 Dieter Kaiser
 ;;;
 ;;; This program is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU Lesser General Public License for Lisp
@@ -52,31 +52,32 @@
 ;;; struct GdkFrameTimings
 ;;; ----------------------------------------------------------------------------
 
+(glib-init::at-init () (foreign-funcall "gdk_frame_timings_get_type" g-size))
+
 (define-g-boxed-opaque gdk-frame-timings "GdkFrameTimings"
   :alloc (error "GdkFrameTimings can not be created from Lisp side."))
 
 #+cl-cffi-gtk-documentation
 (setf (gethash 'gdk-frame-timings atdoc:*class-name-alias*) "CStruct"
       (documentation 'gdk-frame-timings 'type)
- "@version{2016-1-3}
+ "@version{2020-11-12}
   @begin{short}
-    A @sym{gdk-frame-timings} object holds timing information for a single frame
-    of the application’s displays.
+    A @sym{gdk-frame-timings} structure holds timing information for a single
+    frame of the application’s displays.
   @end{short}
-  To retrieve @sym{gdk-frame-timings} objects, use the functions
-  @fun{gdk-frame-clock-get-timings} or
-  @fun{gdk-frame-clock-get-current-timings}. The information in
-  @sym{gdk-frame-timings} is useful for precise synchronization of video with
-  the event or audio streams, and for measuring quality metrics for the
-  application’s display, such as latency and jitter.
+  To retrieve a @sym{gdk-frame-timings} structure, use the functions
+  @fun{gdk-frame-clock-timings} or @fun{gdk-frame-clock-current-timings}. The
+  information in @sym{gdk-frame-timings} structures is useful for precise
+  synchronization of video with the event or audio streams, and for measuring
+  quality metrics for the application’s display, such as latency and jitter.
   @begin{pre}
 (define-g-boxed-opaque gdk-frame-timings \"GdkFrameTimings\"
   :alloc (error \"GdkFrameTimings can not be created from Lisp side.\"))
   @end{pre}
   Since 3.8
   @see-class{gdk-frame-clock}
-  @see-function{gdk-frame-clock-get-timings}
-  @see-function{gdk-frame-clock-get-current-timings}")
+  @see-function{gdk-frame-clock-timings}
+  @see-function{gdk-frame-clock-current-timings}")
 
 (export (boxed-related-symbols 'gdk-frame-timings))
 
@@ -117,144 +118,171 @@
 ;;; ----------------------------------------------------------------------------
 
 ;;; ----------------------------------------------------------------------------
-;;; gdk_frame_timings_get_frame_counter ()
-;;;
-;;; gint64
-;;; gdk_frame_timings_get_frame_counter (GdkFrameTimings *timings);
-;;;
-;;; Gets the frame counter value of the GdkFrameClock when this this frame was
-;;; drawn.
-;;;
-;;; Parameters
-;;;
-;;; timings
-;;;     a GdkFrameTimings
-;;;
-;;; Returns
-;;;     the frame counter value for this frame
-;;;
-;;; Since 3.8
+;;; gdk_frame_timings_get_frame_counter () -> gdk-frame-timings-frame-counter
 ;;; ----------------------------------------------------------------------------
 
+(defcfun ("gdk_frame_timings_get_frame_counter" gdk-frame-timings-frame-counter)
+    :int64
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-12}
+  @argument[timings]{a @class{gdk-frame-timings} structure}
+  @return{The integer frame counter value for this frame.}
+  @begin{short}
+    Gets the frame counter value of the frame clock when this frame was drawn.
+  @end{short}
+
+  Since 3.8
+  @see-class{gdk-frame-timings}"
+  (timings (g-boxed-foreign gdk-frame-timings)))
+
+(export 'gdk-frame-timings-frame-counter)
+
 ;;; ----------------------------------------------------------------------------
-;;; gdk_frame_timings_get_complete ()
-;;;
-;;; gboolean
-;;; gdk_frame_timings_get_complete (GdkFrameTimings *timings);
-;;;
-;;; The timing information in a GdkFrameTimings is filled in incrementally as
-;;; the frame as drawn and passed off to the window system for processing and
-;;; display to the user. The accessor functions for GdkFrameTimings can return 0
-;;; to indicate an unavailable value for two reasons: either because the
-;;; information is not yet available, or because it isn't available at all. Once
-;;; gdk_frame_timings_get_complete() returns TRUE for a frame, you can be
-;;; certain that no further values will become available and be stored in the
-;;; GdkFrameTimings.
-;;;
-;;; Parameters
-;;;
-;;; timings
-;;;     a GdkFrameTimings
-;;;
-;;; Returns
-;;;     TRUE if all information that will be available for the frame has been
-;;;     filled in.
-;;;
-;;; Since: 3.8
+;;; gdk_frame_timings_get_complete () -> gdk-frame-timings-complete
 ;;;-----------------------------------------------------------------------------
 
-;;; ----------------------------------------------------------------------------
-;;; gdk_frame_timings_get_frame_time ()
-;;;
-;;; gint64
-;;; gdk_frame_timings_get_frame_time (GdkFrameTimings *timings);
-;;;
-;;; Returns the frame time for the frame. This is the time value that is
-;;; typically used to time animations for the frame. See
-;;; gdk_frame_clock_get_frame_time().
-;;;
-;;; Parameters
-;;;
-;;; timings
-;;;     A GdkFrameTimings
-;;;
-;;; Returns
-;;;     the frame time for the frame, in the timescale of g_get_monotonic_time()
-;;;
-;;; Since 3.8
-;;; ----------------------------------------------------------------------------
+(defcfun ("gdk_frame_timings_get_complete" gdk-frame-timings-complete) :boolean
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-12}
+  @argument[timings]{a @class{gdk-frame-timings} structure}
+  @begin{return}
+    @em{True} if all information that will be available for the frame has been
+    filled in.
+  @end{return}
+  @begin{short}
+    The timing information in a @class{gdk-frame-timings} structure is filled in
+    incrementally as the frame as drawn and passed off to the window system for
+    processing and display to the user.
+  @end{short}
+  The accessor functions for @class{gdk-frame-timings} structures can return 0
+  to indicate an unavailable value for two reasons: either because the
+  information is not yet available, or because it is not available at all. Once
+  the function @sym{gdk-frame-timings-complete} returns @em{true} for a frame,
+  you can be certain that no further values will become available and be stored
+  in the @class{gdk-frame-timings} structure.
+
+  Since 3.8
+  @see-class{gdk-frame-timings}"
+  (timings (g-boxed-foreign gdk-frame-timings)))
+
+(export 'gdk-frame-timings-complete)
 
 ;;; ----------------------------------------------------------------------------
-;;; gdk_frame_timings_get_presentation_time ()
-;;;
-;;; gint64
-;;; gdk_frame_timings_get_presentation_time (GdkFrameTimings *timings);
-;;;
-;;; Returns the presentation time. This is the time at which the frame became
-;;; visible to the user.
-;;;
-;;; Parameters
-;;;
-;;; timings
-;;;     a GdkFrameTimings
-;;;
-;;; Returns
-;;;     the time the frame was displayed to the user, in the timescale of
-;;;     g_get_monotonic_time(), or 0 if no presentation time is available. See
-;;;     gdk_frame_timings_get_complete()
-;;;
-;;; Since 3.8
+;;; gdk_frame_timings_get_frame_time () -> gdk-frame-timings-frame-time
 ;;; ----------------------------------------------------------------------------
+
+(defcfun ("gdk_frame_timings_get_frame_time" gdk-frame-timings-frame-time)
+    :int64
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-12}
+  @argument[timings]{a @class{gdk-frame-timings} structure}
+  @begin{return}
+    The frame time for the frame, in the timescale of the function
+    @fun{g-get-monotonic-time}.
+  @end{return}
+  @begin{short}
+    Returns the frame time for the frame.
+  @end{short}
+  This is the time value that is typically used to time animations for the
+  frame. See the function @fun{gdk-frame-clock-frame-time}.
+
+  Since 3.8
+  @see-class{gdk-frame-timings}
+  @see-function{g-get-monotonic-time}
+  @see-function{gdk-frame-clock-frame-time}"
+  (timings (g-boxed-foreign gdk-frame-timings)))
+
+(export 'gdk-frame-timings-frame-time)
+
+;;; ----------------------------------------------------------------------------
+;;; gdk_frame_timings_get_presentation_time () -> gdk-frame-presentation-time
+;;; ----------------------------------------------------------------------------
+
+(defcfun ("gdk_frame_timings_get_presentation_time"
+           gdk-frame-timings-presentation-time) :int64
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-12}
+  @argument[timings]{a @class{gdk-frame-timings} structure}
+  @begin{return}
+    The time the frame was displayed to the user, in the timescale of the
+    function @fun{g-get-monotonic-time}, or 0 if no presentation time is
+    available. See the function @fun{gdk-frame-timings-complete}.
+  @end{return}
+  @begin{short}
+    Returns the presentation time.
+  @end{short}
+  This is the time at which the frame became visible to the user.
+
+  Since 3.8
+  @see-class{gdk-frame-timings}
+  @see-function{g-get-monotonic-time}
+  @see-function{gdk-frame-timings-complete}"
+  (timings (g-boxed-foreign gdk-frame-timings)))
+
+(export 'gdk-frame-timings-presentation-time)
 
 ;;; ----------------------------------------------------------------------------
 ;;; gdk_frame_timings_get_refresh_interval ()
-;;;
-;;; gint64
-;;; gdk_frame_timings_get_refresh_interval (GdkFrameTimings *timings);
-;;;
-;;; Gets the natural interval between presentation times for the display that
-;;; this frame was displayed on. Frame presentation usually happens during the
-;;; "vertical blanking interval".
-;;;
-;;; Parameters
-;;;
-;;; timings
-;;;     a GdkFrameTimings
-;;;
-;;; Returns
-;;;     the refresh interval of the display, in microseconds, or 0 if the
-;;;     refresh interval is not available. See gdk_frame_timings_get_complete().
-;;;
-;;; Since 3.8
+;;; -> gdk-frame-timings-refresh-interval
 ;;; ----------------------------------------------------------------------------
+
+(defcfun ("gdk_frame_timings_get_refresh_interval"
+           gdk-frame-timings-refresh-interval) :int64
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-12}
+  @argument[timings]{a @class{gdk-frame-timings} structure}
+  @begin{return}
+    An integer with the refresh interval of the display, in microseconds, or 0
+    if the refresh interval is not available. See the function
+    @fun{gdk-frame-timings-complete}.
+  @end{return}
+  @begin{short}
+    Gets the natural interval between presentation times for the display that
+    this frame was displayed on.
+  @end{short}
+  Frame presentation usually happens during the \"vertical blanking interval\".
+
+  Since 3.8
+  @see-class{gdk-frame-timings}
+  @see-function{gdk-frame-timings-complete}"
+  (timings (g-boxed-foreign gdk-frame-timings)))
+
+(export 'gdk-frame-timings-refresh-interval)
 
 ;;; ----------------------------------------------------------------------------
 ;;; gdk_frame_timings_get_predicted_presentation_time ()
-;;;
-;;; gint64
-;;; gdk_frame_timings_get_predicted_presentation_time (GdkFrameTimings *timings)
-;;;
-;;; Gets the predicted time at which this frame will be displayed. Although no
-;;; predicted time may be available, if one is available, it will be available
-;;; while the frame is being generated, in contrast to
-;;; gdk_frame_timings_get_presentation_time(), which is only available after the
-;;; frame has been presented. In general, if you are simply animating, you
-;;; should use gdk_frame_clock_get_frame_time() rather than this function, but
-;;; this function is useful for applications that want exact control over
-;;; latency. For example, a movie player may want this information for
-;;; Audio/Video synchronization.
-;;;
-;;; Parameters
-;;;
-;;; timings
-;;;     a GdkFrameTimings
-;;;
-;;; Returns
-;;;     The predicted time at which the frame will be presented, in the
-;;;     timescale of g_get_monotonic_time(), or 0 if no predicted presentation
-;;;     time is available.
-;;;
-;;; Since 3.8
+;;; -> gdk-frame-timings-predicted-presentation-time
 ;;; ----------------------------------------------------------------------------
+
+(defcfun ("gdk_frame_timings_get_predicted_presentation_time"
+           gdk-frame-timings-predicted-presentation-time) :int64
+ #+cl-cffi-gtk-documentation
+ "@version{2020-11-12}
+  @argument[timings]{a @class{gdk-frame-timings} structure}
+  @begin{return}
+    An integer with the predicted time at which the frame will be presented, in
+    the timescale of the function @fun{g-get-monotonic-time}, or 0 if no
+    predicted presentation time is available.
+  @end{return}
+  @begin{short}
+    Gets the predicted time at which this frame will be displayed.
+  @end{short}
+  Although no predicted time may be available, if one is available, it will be
+  available while the frame is being generated, in contrast to the function
+  @fun{gdk-frame-timings-presentation-time}, which is only available after the
+  frame has been presented. In general, if you are simply animating, you should
+  use the function @fun{gdk-frame-clock-frame-time} rather than this function,
+  but this function is useful for applications that want exact control over
+  latency. For example, a movie player may want this information for Audio/Video
+  synchronization.
+
+  Since 3.8
+  @see-class{gdk-frame-timings}
+  @see-function{g-get-monotonic-time}
+  @see-function{gdk-frame-clock-frame-time}
+  @see-function{gdk-frame-timings-presentation-time}"
+  (timings (g-boxed-foreign gdk-frame-timings)))
+
+(export 'gdk-frame-timings-predicted-presentation-time)
 
 ;;; --- End of file gdk.frame-timings.lisp -------------------------------------
