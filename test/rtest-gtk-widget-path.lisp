b613a266a5f8e7f477b66a33d4df84fbed3dc7bc
crategus <crategus@users.sourceforge.net>
2021-07-25 22:43:20 +0200

    Updating the tests for GtkWidgetPath

--- test/rtest-gtk-widget-path.lisp
+++ test/rtest-gtk-widget-path.lisp
@@ -72,36 +72,74 @@
                   (gtk-widget-path-append-type path "GtkNotebook")
                   "tab"
                   '(:even :first)))
-    (is (string= "GtkNotebook tab:even:first" (gtk-widget-path-to-string path)))))
+    (is (string= "GtkNotebook tab:even:first"
+                 (gtk-widget-path-to-string path)))))
 
 ;;;     gtk_widget_path_iter_clear_classes
 ;;;     gtk_widget_path_iter_clear_regions
 
 ;;;     gtk_widget_path_iter_get_name
+;;;     gtk_widget_path_iter_set_name
 
-(test gtk-widget-path-iter-get-name
+(test gtk-widget-path-iter-name.1
   (let* ((widget (make-instance 'gtk-button))
          (path (gtk-widget-path widget)))
     ;; TODO: Why is this false.
-    (is-false (gtk-widget-path-iter-get-name path -1))))
+    (is-false (gtk-widget-path-iter-name path -1))))
+
+(test gtk-widget-path-iter-name.2
+  (let ((path (gtk-widget-path-new)))
+    (is-true (setf (gtk-widget-path-iter-name
+                        path
+                        (gtk-widget-path-append-type path "GtkLabel"))
+                   "first tab label"))
+    (is (string= "GtkLabel(first tab label)"
+                 (gtk-widget-path-to-string path)))))
 
 ;;;     gtk_widget_path_iter_get_object_name
 
 (test gtk-widget-path-iter-get-object-name
   (let* ((widget (make-instance 'gtk-button))
          (path (gtk-widget-path widget)))
-    (is (string= "button" (gtk-widget-path-iter-get-object-name path -1)))))
+    (is (string= "button" (gtk-widget-path-iter-object-name path -1)))))
 
 ;;;     gtk_widget_path_iter_get_object_type
 
 (test gtk-widget-path-iter-object-type
   (let* ((widget (make-instance 'gtk-button))
          (path (gtk-widget-path widget)))
-    (is (string= "GtkButton" (g-type-name (gtk-widget-path-iter-object-type path -1))))))
+    (is (string= "GtkButton"
+                 (g-type-name (gtk-widget-path-iter-object-type path -1))))))
 
 ;;;     gtk_widget_path_iter_get_siblings
 ;;;     gtk_widget_path_iter_get_sibling_index
+
 ;;;     gtk_widget_path_iter_get_state
+;;;     gtk_widget_path_iter_set_state
+
+(test gtk-widget-path-iter-state
+  (let ((path (gtk-widget-path-new)))
+    (is (= 0 (gtk-widget-path-append-type path "GtkButton")))
+    (is (string= "GtkButton" (gtk-widget-path-to-string path)))
+    (is (equal '(:active :dir-ltr)
+               (setf (gtk-widget-path-iter-state path 0)
+                     '(:active :dir-ltr))))
+    (is (equal '(:active :dir-ltr) (gtk-widget-path-iter-state path 0)))
+    (is (eq  :active (setf (gtk-widget-path-iter-state path 0) :active)))
+    (is (equal '(:active) (gtk-widget-path-iter-state path 0)))
+    ;; Set more flags
+    (let ((flags (gtk-widget-path-iter-state path 0)))
+      (is-true (setf (gtk-widget-path-iter-state path 0)
+                     (union flags '(:dir-ltr :selected))))
+      (is (equal '(:active :selected :dir-ltr)
+                 (gtk-widget-path-iter-state path 0))))
+    ;; Unset a flag
+    (let ((flags (gtk-widget-path-iter-state path 0)))
+      (is-true (setf (gtk-widget-path-iter-state path 0)
+                     (set-difference flags '(:active))))
+      (is (equal '(:selected :dir-ltr)
+                 (gtk-widget-path-iter-state path 0))))))
+
 ;;;     gtk_widget_path_iter_has_class
 ;;;     gtk_widget_path_iter_has_name
 ;;;     gtk_widget_path_iter_has_qclass
@@ -118,48 +156,16 @@
                   "tab"
                   '(:even :first)))
     (is (string= "GtkNotebook tab:even:first" (gtk-widget-path-to-string path)))
-    (is (equal '(:even :first) (gtk-widget-path-iter-has-region path -1 "tab")))))
+    (is (equal '(:even :first)
+               (gtk-widget-path-iter-has-region path -1 "tab")))))
 
 ;;;     gtk_widget_path_iter_list_classes
 ;;;     gtk_widget_path_iter_list_regions
 ;;;     gtk_widget_path_iter_remove_class
 ;;;     gtk_widget_path_iter_remove_region
 
-;;;     gtk_widget_path_iter_set_name
-
-(test gtk-widget-path-iter-set-name
-  (let ((path (gtk-widget-path-new)))
-    (is-false (gtk-widget-path-iter-set-name
-                  path
-                  (gtk-widget-path-append-type path "GtkLabel")
-                  "first tab label"))
-    (is (string= "GtkLabel(first tab label)" (gtk-widget-path-to-string path)))))
-
 ;;;     gtk_widget_path_iter_set_object_name
 ;;;     gtk_widget_path_iter_set_object_type
-
-;;;     gtk_widget_path_iter_set_state
-
-(test gtk-widget-path-iter-set-state
-  (let ((path (gtk-widget-path-new)))
-    (is (= 0 (gtk-widget-path-append-type path "GtkButton")))
-    (is (string= "GtkButton" (gtk-widget-path-to-string path)))
-    (is-false (gtk-widget-path-iter-set-state path 0 '(:active :dir-ltr)))
-    (is (equal '(:active :dir-ltr) (gtk-widget-path-iter-get-state path 0)))
-    (is-false (gtk-widget-path-iter-set-state path 0 :active))
-    (is (equal '(:active) (gtk-widget-path-iter-get-state path 0)))
-    ;; Set flags in addition
-    (let ((flags (gtk-widget-path-iter-get-state path 0)))
-      (is-false (gtk-widget-path-iter-set-state path
-                                                0
-                                                (union flags '(:dir-ltr :selected))))
-      (is (equal '(:active :selected :dir-ltr) (gtk-widget-path-iter-get-state path 0))))
-    ;; Unset a flag
-    (let ((flags (gtk-widget-path-iter-get-state path 0)))
-      (is-false (gtk-widget-path-iter-set-state path
-                                                0
-                                                (set-difference flags '(:active))))
-      (is (equal '(:selected :dir-ltr) (gtk-widget-path-iter-get-state path 0))))))
 
 ;;;     gtk_widget_path_length
 ;;;     gtk_widget_path_new
@@ -173,4 +179,4 @@
     (is (= 1 (gtk-widget-path-append-type path "GtkButton")))
     (is (string= "GtkWindow GtkButton" (gtk-widget-path-to-string path)))))
 
-;;; 2020-11-13
+;;; 2021-7-25
