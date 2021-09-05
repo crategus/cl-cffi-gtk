b6c98603d3fc9281814129e709fd5240a477a8a6
crategus <crategus@users.sourceforge.net>
2021-09-05 19:58:09 +0200

    Updating the test for GIcon

--- test/rtest-gio-icon.lisp
+++ test/rtest-gio-icon.lisp
@@ -1,5 +1,5 @@
-(def-suite gio-icon :in gio-suite)
-(in-suite gio-icon)
+(def-suite g-icon :in gio-suite)
+(in-suite g-icon)
 
 ;;;     GIcon
 
@@ -21,20 +21,14 @@
                        (:EXPORT T))
              (get-g-type-definition "GIcon"))))
 
-;;;     GIconIface
-
 ;;;   g_icon_hash
 
 (test g-icon-hash
   (let* ((names (list "gnome-dev-cdrom-audio"))
          (icon1 (g-themed-icon-new-from-names names))
          (icon2 (g-themed-icon-new-with-default-fallbacks "gnome-dev-cdrom-audio")))
-;    (is (equal (g-themed-icon-names icon1)
-;               (g-themed-icon-names icon2)))
     (is (= 1275220063 (g-icon-hash icon1)))
-    (is (= 2604122446 (g-icon-hash icon2)))
-;    (is (= (g-icon-hash icon1) (g-icon-hash icon2)))
-))
+    (is (= 2604122446 (g-icon-hash icon2)))))
 
 ;;;   g_icon_equal
 
@@ -42,8 +36,6 @@
   (let* ((names (list "gnome-dev-cdrom-audio" "gnome-dev-cdrom" "gnome-dev" "gnome"))
          (icon1 (g-themed-icon-new-from-names names))
          (icon2 (g-themed-icon-new-with-default-fallbacks "gnome-dev-cdrom-audio")))
-;    (is (equal (g-themed-icon-names icon1)
-;               (g-themed-icon-names icon2)))
     (is-true (g-icon-equal icon1 icon2))))
 
 ;;;   g_icon_to_string
@@ -53,24 +45,39 @@
          (icon1 (g-themed-icon-new "gnome-dev-cdrom-audio"))
          (icon2 (g-themed-icon-new-from-names names))
          (icon3 (g-themed-icon-new-with-default-fallbacks "gnome-dev-cdrom-audio")))
-    (is (equal "gnome-dev-cdrom-audio"
-               (g-icon-to-string icon1)))
-    (is (equal ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
-               (g-icon-to-string icon2)))
-    (is (equal ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
-               (g-icon-to-string icon3)))))
+    (is (string= "gnome-dev-cdrom-audio" (g-icon-to-string icon1)))
+    (is (string= ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
+                 (g-icon-to-string icon2)))
+    (is (string= ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
+                 (g-icon-to-string icon3)))))
 
 ;;;   g_icon_new_for_string
 
 (test g-icon-new-for-string
   (let ((icon1 (g-icon-new-for-string "gnome-dev-cdrom-audio"))
-        (icon2 (g-icon-new-for-string ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome"))
+        (icon2 (g-icon-new-for-string ". GThemedIcon gnome-dev-cdrom-audio
+                                       gnome-dev-cdrom gnome-dev gnome"))
         (icon3 (g-themed-icon-new-with-default-fallbacks "gnome-dev-cdrom-audio")))
-    (is (equal "gnome-dev-cdrom-audio"
-               (g-icon-to-string icon1)))
-    (is (equal ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
-               (g-icon-to-string icon2)))
-    (is (equal ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
-               (g-icon-to-string icon3)))))
+    (is (string= "gnome-dev-cdrom-audio" (g-icon-to-string icon1)))
+    (is (string= ". GThemedIcon gnome-dev-cdrom-audio%0A  gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio%0A-symbolic -symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
+                 (g-icon-to-string icon2)))
+    (is (string= ". GThemedIcon gnome-dev-cdrom-audio gnome-dev-cdrom gnome-dev gnome gnome-dev-cdrom-audio-symbolic gnome-dev-cdrom-symbolic gnome-dev-symbolic gnome-symbolic"
+                 (g-icon-to-string icon3)))))
 
-;;; 2021-4-15
+;;;     g_icon_serialize
+
+(test g-icon-serialize
+  (let ((icon (g-icon-new-for-string "gnome-dev-cdrom-audio")))
+    (is (pointerp (g-icon-serialize icon)))
+    (is (string= "('themed', <['gnome-dev-cdrom-audio', 'gnome-dev-cdrom-audio-symbolic']>)"
+                 (g-variant-print (g-icon-serialize icon))))))
+
+;;;     g_icon_deserialize
+
+(test g-icon-deserialize
+  (let* ((icon (g-icon-new-for-string "gnome-dev-cdrom-audio"))
+         (value (g-icon-serialize icon)))
+    (is (pointerp value))
+    (is (typep (g-icon-deserialize value) 'g-icon))))
+
+;;; 2021-9-5
