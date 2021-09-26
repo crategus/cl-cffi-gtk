1f225ca179a6eaeba96ceba41cfce9810a80dbd9
crategus <crategus@users.sourceforge.net>
2021-09-26 13:03:53 +0200

    Remove the deprecated gtk-widget-pointer function

--- demo/gtk-example/widget-pointer.lisp
+++ demo/gtk-example/widget-pointer.lisp
@@ -1,3 +1,5 @@
+;;;; Example Widget Pointer - 2021-9-25
+
 (in-package :gtk-example)
 
 (defun create-event-box ()
@@ -7,28 +9,26 @@
     (setf (gtk-widget-events event-box) :button-press-mask)
     ;; Connect a signal to the event box
     (g-signal-connect event-box "button-press-event"
-                      (lambda (box event)
-                        ;; x,y for pointer from event
-                        (format t "~%Event box clicked at   : ~6,2f, ~6,2f~%"
-                                  (gdk-event-button-x event)
-                                  (gdk-event-button-y event))
-                        ;; x,y for pointer from widget (deprectated)
-                        (multiple-value-bind (x y)
-                            (gtk-widget-pointer box)
-                          (format t "Pointer position       : ~d, ~d~%" x y))
-                        (let* ((seat (gdk-display-default-seat (gdk-display-default)))
-                               (device (gdk-seat-pointer seat)))
-                          ;; x,y, for pointer from device position
-                          (multiple-value-bind (win x y mask)
-                              (gdk-window-device-position (gtk-widget-window event-box) device)
-                            (declare (ignore win mask))
-                            (format t "Device Position        : ~d, ~d~%" x y))
-                          ;; x,y for pointer from device position in double precision
-                          (multiple-value-bind (win x y mask)
-                              (gdk-window-device-position-double (gtk-widget-window event-box) device)
-                            (declare (ignore win mask))
-                            (format t "Device Position double : ~6,2f, ~6,2f~%" x y)))
-                        +gdk-event-stop+))
+        (lambda (box event)
+          (declare (ignore box))
+          ;; x,y for pointer from event
+          (format t "~%Event box clicked at   : ~6,2f, ~6,2f~%"
+                    (gdk-event-button-x event)
+                    (gdk-event-button-y event))
+          (let* ((seat (gdk-display-default-seat (gdk-display-default)))
+                 (device (gdk-seat-pointer seat))
+                 (window (gtk-widget-window event-box)))
+            ;; x,y, for pointer from device position
+            (multiple-value-bind (win x y mask)
+                (gdk-window-device-position window device)
+              (declare (ignore win mask))
+              (format t "Device Position        : ~d, ~d~%" x y))
+            ;; x,y for pointer from device position in double precision
+            (multiple-value-bind (win x y mask)
+                (gdk-window-device-position-double window device)
+              (declare (ignore win mask))
+              (format t "Device Position double : ~6,2f, ~6,2f~%" x y)))
+          +gdk-event-stop+))
     ;; Add the image to the event box
     (gtk-container-add event-box image)
     ;; Return the event box with the image
@@ -36,7 +36,9 @@
 
 (defun example-widget-pointer ()
   (within-main-loop
-    (let ((window (gtk-window-new :toplevel)))
+    (let ((window (make-instance 'gtk-window
+                                 :type :toplevel
+                                 :title "Example Widget Pointer")))
       ;; Signal handler for the window to handle the signal "destroy".
       (g-signal-connect window "destroy"
                         (lambda (widget)
@@ -46,4 +48,3 @@
       (gtk-container-add window (create-event-box))
       ;; Show the window.
       (gtk-widget-show-all window))))
-
